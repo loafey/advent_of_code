cad2dc481fc13a4c3f362789304cd37ddfba6c7d
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-10-19 21:27:24 +0200

    Finished day 3.

---
+++ src/aoc_2018/day3.rs
@@ -0,0 +1,110 @@
+use std::collections::{HashMap, HashSet};
+
+pub fn part1() -> usize {
+    let mut map = HashMap::new();
+    include_str!("input/day3.input").split('\n').for_each(|s| {
+        let mut split = s.split_whitespace().skip(2);
+        let coord = split
+            .next()
+            .map(|s| {
+                let mut split = s.split(|c| c == ':' || c == ',');
+                (
+                    split.next().and_then(|s| s.parse::<usize>().ok()).unwrap(),
+                    split.next().and_then(|s| s.parse::<usize>().ok()).unwrap(),
+                )
+            })
+            .unwrap();
+        let size = split
+            .next()
+            .map(|s| {
+                let mut split = s.split('x');
+                (
+                    split.next().and_then(|s| s.parse::<usize>().ok()).unwrap(),
+                    split.next().and_then(|s| s.parse::<usize>().ok()).unwrap(),
+                )
+            })
+            .unwrap();
+
+        for x in coord.0..coord.0 + size.0 {
+            for y in coord.1..coord.1 + size.1 {
+                if let Some(c) = map.get_mut(&(x, y)) {
+                    *c += 1;
+                } else {
+                    map.insert((x, y), 1);
+                }
+            }
+        }
+    });
+    map.into_iter().filter(|(_, v)| v > &1).count()
+}
+
+pub fn part2() -> usize {
+    #[derive(Debug, PartialEq, Eq, Hash)]
+    struct Fabric {
+        id: usize,
+        coord: (usize, usize),
+        size: (usize, usize),
+    }
+    impl Fabric {
+        fn collides(&self, other: &Self) -> bool {
+            self.coord.0 < other.coord.0 + other.size.0
+                && self.coord.0 + self.size.0 > other.coord.0
+                && self.coord.1 < other.coord.1 + other.size.1
+                && self.size.1 + self.coord.1 > other.coord.1
+        }
+    }
+
+    let map = include_str!("input/day3.input")
+        .split('\n')
+        .map(|s| {
+            let mut split = s.split_whitespace();
+            let id = split
+                .next()
+                .and_then(|s| s[1..].parse::<usize>().ok())
+                .unwrap();
+            split.next();
+            let coord = split
+                .next()
+                .map(|s| {
+                    let mut split = s.split(|c| c == ':' || c == ',');
+                    (
+                        split.next().and_then(|s| s.parse::<usize>().ok()).unwrap(),
+                        split.next().and_then(|s| s.parse::<usize>().ok()).unwrap(),
+                    )
+                })
+                .unwrap();
+            let size = split
+                .next()
+                .map(|s| {
+                    let mut split = s.split('x');
+                    (
+                        split.next().and_then(|s| s.parse::<usize>().ok()).unwrap(),
+                        split.next().and_then(|s| s.parse::<usize>().ok()).unwrap(),
+                    )
+                })
+                .unwrap();
+            Fabric { id, coord, size }
+        })
+        .collect::<Vec<_>>();
+
+    let mut clean = HashSet::new();
+    for f1 in &map {
+        let mut no_collide = true;
+        for f2 in &map {
+            if f1 == f2 {
+                continue;
+            }
+
+            if f1.collides(f2) {
+                clean.remove(f1);
+                clean.remove(f2);
+                no_collide = false;
+            }
+        }
+        if no_collide {
+            clean.insert(f1);
+        }
+    }
+
+    clean.iter().next().unwrap().id
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use aoc_table::table_gen::TableGen;
 mod aoc_2018;
 
 fn main() {
-    //AOC_2018::day1::part2();
     TableGen::new("2018 solutions")
         .add(aoc_2018::day1::part1, aoc_2018::day1::part2)
         .add(aoc_2018::day2::part1, aoc_2018::day2::part2)
+        .add(aoc_2018::day3::part1, aoc_2018::day3::part2)
         .run();
 }
