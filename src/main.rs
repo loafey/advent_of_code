5c926be099d6614d7bdf98fcbc893b47e0cfa883
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-10-19 20:45:40 +0200

    Completed day 2 .

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ fn main() {
     //AOC_2018::day1::part2();
     TableGen::new("2018 solutions")
         .add(aoc_2018::day1::part1, aoc_2018::day1::part2)
+        .add(aoc_2018::day2::part1, aoc_2018::day2::part2)
         .run();
 }
