96b25c04080a706a22463f9818bd8e2a5060cff2
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-10-19 20:17:26 +0200

    Finished day 1.

---
+++ src/aoc_2018/day1.rs
@@ -0,0 +1,37 @@
+use std::collections::HashSet;
+
+pub fn part1() -> i32 {
+    let input = include_str!("input/day1.input");
+    input
+        .split('\n')
+        .map(|s| {
+            let op = &s[0..1];
+            s[1..].parse::<i32>().unwrap() * if op == "-" { -1 } else { 1 }
+        })
+        .sum()
+}
+pub fn part2() -> i32 {
+    let input = include_str!("input/day1.input");
+    let mut freq = 0;
+    let mut list = HashSet::new();
+    list.insert(freq);
+
+    let parsed = input
+        .split('\n')
+        .map(|s| {
+            let op = &s[0..1];
+            s[1..].parse::<i32>().unwrap() * if op == "-" { -1 } else { 1 }
+        })
+        .cycle();
+
+    for s in parsed {
+        freq += s;
+        if !list.contains(&freq) {
+            list.insert(freq);
+        } else {
+            break;
+        }
+    }
+
+    freq
+}
