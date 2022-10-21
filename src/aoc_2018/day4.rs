b62cfeb2d48d3a9c9a15bafbb631ab9db106d87e
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-10-21 13:27:12 +0200

    Day 4 is very wrongo.

--- src/aoc_2018/day4.rs
+++ src/aoc_2018/day4.rs
@@ -1,17 +1,21 @@
-use std::collections::{BTreeMap, HashMap};
+use std::{
+    collections::{BTreeMap, HashMap},
+    ops::{Range, RangeInclusive},
+};
 
 use crate::utils::parse_next;
 
-pub fn part1() -> i32 {
+fn parse_sleep_ranges() -> HashMap<i32, Vec<Range<i32>>> {
     enum Event {
         GuardBegin(i32),
         FallAsleep,
         WakeUp,
     }
 
+    let mut sleep_ranges: HashMap<_, Vec<_>> = HashMap::new();
+
     let mut current_guard = 0;
     let mut current_start = 0;
-    let mut sleep_ranges: HashMap<_, Vec<_>> = HashMap::new();
     include_str!("input/day4.input")
         .split('\n')
         .map(|s| {
@@ -41,19 +45,25 @@ pub fn part1() -> i32 {
             Event::FallAsleep => current_start = t,
             Event::WakeUp => {
                 if let Some(g) = sleep_ranges.get_mut(&current_guard) {
-                    g.push(current_start..=t)
+                    g.push(current_start..t)
                 } else {
-                    sleep_ranges.insert(current_guard, vec![current_start..=t]);
+                    sleep_ranges.insert(current_guard, vec![current_start..t]);
                 }
             }
         });
+
+    sleep_ranges
+}
+
+pub fn part1() -> i32 {
+    let mut sleep_ranges = parse_sleep_ranges();
 
     let most_asleep = {
         let mut guards = BTreeMap::new();
         for (guard, sleep_times) in &sleep_ranges {
             guards.insert(
                 guard,
-                sleep_times.iter().map(|s| s.end() - s.start()).sum::<i32>(),
+                sleep_times.iter().map(|s| s.end - s.start).sum::<i32>(),
             );
         }
         let mut guards = guards
@@ -74,7 +84,6 @@ pub fn part1() -> i32 {
         }
         let mut largest_val = 0;
         let mut largest_index = 0;
-        println!("{:?}", minutes.iter().enumerate().collect::<Vec<_>>());
         (0..minutes.len()).for_each(|i| {
             if minutes[i] >= largest_val {
                 largest_val = minutes[i];
@@ -84,11 +93,37 @@ pub fn part1() -> i32 {
         largest_index
     };
 
-    println!("{} {}", most_asleep, big_minute);
-
     most_asleep * big_minute as i32
 }
 
 pub fn part2() -> i32 {
-    0
+    let sleep_ranges = parse_sleep_ranges()
+        .into_iter()
+        .map(|(k, r)| {
+            let mut minutes = [0; 60];
+            for range in r {
+                for v in range {
+                    minutes[v as usize] += 1;
+                }
+            }
+            (k, minutes)
+        })
+        .collect::<HashMap<_, _>>();
+    let mut k = sleep_ranges
+        .into_iter()
+        .map(|(k, v)| {
+            let mut biggest = 0;
+            let mut biggest_index = 0;
+            (0..v.len()).for_each(|i| {
+                if v[i] > biggest {
+                    biggest = v[i];
+                    biggest_index = i;
+                }
+            });
+            (biggest, k * biggest_index as i32)
+        })
+        .collect::<BTreeMap<_, _>>();
+
+    println!("{k:?}");
+    k.pop_last().unwrap().1
 }
