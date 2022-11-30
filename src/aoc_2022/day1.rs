4174493f1c53fd182f57360aa44023c652843bd3
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-11-30 19:51:14 +0100

    Added files for first day of 22.

---
+++ src/aoc_2022/day1.rs
@@ -0,0 +1,7 @@
+pub fn part1() -> i32 {
+    include_str!("input/day1.input");
+}
+
+pub fn part2() -> i32 {
+    include_str!("input/day1.input");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use aoc_table::table_gen::TableGen;
-mod aoc_2018;
+// mod aoc_2018;
+mod aoc_2022;
 mod utils;
 
 fn main() {
     // dbg!(aoc_2018::day4::part2());
-    TableGen::new("2018 solutions")
-        .add(aoc_2018::day1::part1, aoc_2018::day1::part2)
-        .add(aoc_2018::day2::part1, aoc_2018::day2::part2)
-        .add(aoc_2018::day3::part1, aoc_2018::day3::part2)
-        .add(aoc_2018::day4::part1, aoc_2018::day4::part2)
-        .add(aoc_2018::day5::part1, aoc_2018::day5::part2)
-        .add(aoc_2018::day6::part1, aoc_2018::day6::part2)
-        .add(aoc_2018::day7::part1, aoc_2018::day7::part2)
-        .add(aoc_2018::day8::part1, aoc_2018::day8::part2)
-        .add(aoc_2018::day9::part1, aoc_2018::day9::part2)
-        .add(aoc_2018::day10::part1, aoc_2018::day10::part2)
-        .add(aoc_2018::day11::part1, aoc_2018::day11::part2)
-        .add(aoc_2018::day12::part1, aoc_2018::day12::part2)
+    // TableGen::new("2018 solutions")
+    //     .add(aoc_2018::day1::part1, aoc_2018::day1::part2)
+    //     .add(aoc_2018::day2::part1, aoc_2018::day2::part2)
+    //     .add(aoc_2018::day3::part1, aoc_2018::day3::part2)
+    //     .add(aoc_2018::day4::part1, aoc_2018::day4::part2)
+    //     .add(aoc_2018::day5::part1, aoc_2018::day5::part2)
+    //     .add(aoc_2018::day6::part1, aoc_2018::day6::part2)
+    //     .add(aoc_2018::day7::part1, aoc_2018::day7::part2)
+    //     .add(aoc_2018::day8::part1, aoc_2018::day8::part2)
+    //     .add(aoc_2018::day9::part1, aoc_2018::day9::part2)
+    //     .add(aoc_2018::day10::part1, aoc_2018::day10::part2)
+    //     .add(aoc_2018::day11::part1, aoc_2018::day11::part2)
+    //     .add(aoc_2018::day12::part1, aoc_2018::day12::part2)
+    //     .run();
+
+    TableGen::new("2022 solutions")
+        .add(aoc_2022::day1::part1, aoc_2022::day1::part1)
         .run();
 }
