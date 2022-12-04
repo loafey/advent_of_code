2e998e7176e790b88c265962c325556bba40502a
Voxed <voxedg@gmail.com>
2022-12-04 08:40:21 +0100

    Delete bad code

--- src/aoc_2022/day10.rs
+++
@@ -1,7 +0,0 @@
-pub fn part1() -> i32 {
-    0
-}
-
-pub fn part2() -> i32 {
-    0
-}
--- src/utils.rs
+++
@@ -1,8 +0,0 @@
-use std::str::FromStr;
-
-#[allow(unused)]
-pub fn parse_next<'l, T: FromStr>(iter: &mut impl Iterator<Item = &'l str>) -> T {
-    let s = iter.next();
-    s.and_then(|s| s.parse::<T>().ok())
-        .unwrap_or_else(|| panic!("Failed to parse string: \"{s:?}\""))
-}
