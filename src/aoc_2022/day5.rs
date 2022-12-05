95bb45a5e2cd1d4421161ce88ae4548665d473eb
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-05 15:45:16 +0100

    Improved day 5 a lot.

--- src/aoc_2022/day5.rs
+++ src/aoc_2022/day5.rs
@@ -1,17 +1,11 @@
-use crate::utils::parse;
-use std::{
-    collections::{BTreeMap, VecDeque},
-    hash::Hash,
-};
+use crate::utils::{bset_pop_top, bset_push_top, parse};
+use std::collections::{BTreeMap, VecDeque};
 
-fn parse_input() -> (
-    BTreeMap<usize, Vec<char>>,
-    impl Iterator<Item = (usize, usize, usize)>,
-) {
+fn parse_input(func: fn(&mut VecDeque<char>, char)) -> String {
     let mut split = include_str!("input/day5.input").split("\n\n");
     let top = split.next().unwrap();
     let bottom = split.next().unwrap();
-    let stacks = {
+    let mut stacks = {
         const MOD: usize = 1;
         let mut map = BTreeMap::new();
         top.split('\n')
@@ -24,61 +18,40 @@ fn parse_input() -> (
                     .filter(|(_, c)| c.is_alphabetic())
                     .map(|(i, c)| ((i / 4) + 1, c))
             })
-            .for_each(|(i, c)| push_top(&mut map, i, c));
+            .for_each(|(i, c)| bset_push_top(&mut map, i, c));
         map
     };
 
-    let iter = bottom.lines().map(|s| {
-        let splat = s.split(' ').collect::<Vec<_>>();
-        let [_, amount, _, from, _ ,to] = &splat[..] else {unreachable!()};
-        (
-            parse::<usize>(amount),
-            parse::<usize>(from),
-            parse::<usize>(to),
-        )
-    });
+    bottom
+        .lines()
+        .map(|s| {
+            let splat = s.split(' ').collect::<Vec<_>>();
+            let [_, amount, _, from, _ ,to] = &splat[..] else {unreachable!()};
+            (
+                parse::<usize>(amount),
+                parse::<usize>(from),
+                parse::<usize>(to),
+            )
+        })
+        .for_each(|(amount, from, to)| {
+            let mut temp_stack = VecDeque::new();
+            for _ in 0..amount {
+                let moved = bset_pop_top(&mut stacks, &from);
+                func(&mut temp_stack, moved);
+            }
+            stacks.get_mut(&to).unwrap().extend(temp_stack.into_iter());
+        });
 
-    (stacks, iter)
+    stacks
+        .into_iter()
+        .filter_map(|(_, mut v)| v.pop())
+        .collect()
 }
 
 pub fn part1() -> String {
-    let (mut stacks, iter) = parse_input();
-    iter.for_each(|(amount, from, to)| {
-        for _ in 0..amount {
-            let moved = pop_top(&mut stacks, &from);
-            push_top(&mut stacks, to, moved);
-        }
-    });
-    stacks
-        .into_iter()
-        .filter_map(|(_, mut v)| v.pop())
-        .collect()
-}
-
-fn pop_top<K: Eq + Hash + Ord, V>(map: &mut BTreeMap<K, Vec<V>>, key: &K) -> V {
-    map.get_mut(key).unwrap().pop().unwrap()
-}
-
-fn push_top<K: Eq + Hash + Ord, V>(map: &mut BTreeMap<K, Vec<V>>, key: K, val: V) {
-    if let Some(vec) = map.get_mut(&key) {
-        vec.push(val)
-    } else {
-        map.insert(key, vec![val]);
-    }
+    parse_input(VecDeque::push_back)
 }
 
 pub fn part2() -> String {
-    let (mut stacks, iter) = parse_input();
-    iter.for_each(|(amount, from, to)| {
-        let mut temp_stack = VecDeque::new();
-        for _ in 0..amount {
-            let moved = pop_top(&mut stacks, &from);
-            temp_stack.push_front(moved);
-        }
-        stacks.get_mut(&to).unwrap().extend(temp_stack.into_iter());
-    });
-    stacks
-        .into_iter()
-        .filter_map(|(_, mut v)| v.pop())
-        .collect()
+    parse_input(VecDeque::push_front)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Debug, str::FromStr};
+use std::{collections::BTreeMap, fmt::Debug, str::FromStr};
 
 #[allow(unused)]
 pub fn parse_next<'l, T: FromStr>(iter: &mut impl Iterator<Item = &'l str>) -> T {
@@ -9,4 +9,16 @@ pub fn parse_next<'l, T: FromStr>(iter: &mut impl Iterator<Item = &'l str>) -> T
 
 pub fn parse<F: FromStr + Debug>(s: &str) -> F {
     s.parse::<F>().ok().unwrap()
+}
+
+pub fn bset_pop_top<K: Eq + Ord, V>(map: &mut BTreeMap<K, Vec<V>>, key: &K) -> V {
+    map.get_mut(key).unwrap().pop().unwrap()
+}
+
+pub fn bset_push_top<K: Eq + Ord, V>(map: &mut BTreeMap<K, Vec<V>>, key: K, val: V) {
+    if let Some(vec) = map.get_mut(&key) {
+        vec.push(val)
+    } else {
+        map.insert(key, vec![val]);
+    }
 }
