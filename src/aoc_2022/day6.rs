ccf9e3a5587dda048926816a23a0af42f45c4613
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-06 09:48:03 +0100

    Finished day 6.

--- src/aoc_2022/day6.rs
+++ src/aoc_2022/day6.rs
@@ -1,7 +1,27 @@
-pub fn part1() -> i32 {
+fn solver(window_size: usize) -> usize {
+    for c in include_str!("input/day6.input")
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .enumerate()
+        .map(|(i, c)| (i + 1, c))
+        .collect::<Vec<_>>()
+        .windows(window_size)
+    {
+        let mut v = c.to_vec();
+        v.sort_by_key(|(_, c)| *c);
+        v.dedup_by_key(|(_, c)| *c);
+        v.sort_by_key(|(i, _)| *i);
+        if v.len() == window_size {
+            return v.last().unwrap().0;
+        }
+    }
     0
 }
 
-pub fn part2() -> i32 {
-    0
+pub fn part1() -> usize {
+    solver(4)
+}
+
+pub fn part2() -> usize {
+    solver(14)
 }
