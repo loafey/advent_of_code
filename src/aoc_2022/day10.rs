39ab033deb097a5a6c99aa7909593a3c5adb3792
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-10 12:31:10 +0100

    Shorted down a lil bit.

--- src/aoc_2022/day10.rs
+++ src/aoc_2022/day10.rs
@@ -2,41 +2,41 @@ enum Instruction {
     NoOp,
     AddX(i32),
 }
+impl From<Instruction> for (i32, i32) {
+    fn from(val: Instruction) -> (i32, i32) {
+        match val {
+            Instruction::NoOp => (0, 1),
+            Instruction::AddX(o) => (o, 2),
+        }
+    }
+}
+
+fn parse_ops() -> impl Iterator<Item = Instruction> {
+    include_str!("input/day10.input").lines().map(|s| {
+        let mut splat = s.split_whitespace().skip(1);
+        splat
+            .next()
+            .map(|a| Instruction::AddX(a.parse().unwrap()))
+            .unwrap_or(Instruction::NoOp)
+    })
+}
 
 pub fn part1() -> i32 {
     let mut x = 1;
     let mut cycle = 0;
     let mut signal_strenght = 0;
-    include_str!("input/day10.input")
-        .lines()
-        .map(|s| {
-            let mut splat = s.split_whitespace().skip(1);
-            if let Some(a) = splat.next() {
-                Instruction::AddX(a.parse().unwrap())
-            } else {
-                Instruction::NoOp
+    parse_ops().for_each(|i| {
+        let (adder, wait) = i.into();
+        for i in 0..wait {
+            cycle += 1;
+            if [20, 60, 100, 140, 180, 220].contains(&cycle) {
+                signal_strenght += x * cycle;
             }
-        })
-        .for_each(|i| {
-            let mut adder = 0;
-            let wait;
-            match i {
-                Instruction::NoOp => wait = 1,
-                Instruction::AddX(o) => {
-                    adder = o;
-                    wait = 2;
-                }
-            };
-            for i in 0..wait {
-                cycle += 1;
-                if [20, 60, 100, 140, 180, 220].contains(&cycle) {
-                    signal_strenght += x * cycle;
-                }
-                if i == wait - 1 {
-                    x += adder;
-                }
+            if i == wait - 1 {
+                x += adder;
             }
-        });
+        }
+    });
     signal_strenght
 }
 
@@ -44,41 +44,23 @@ pub fn part2() -> &'static str {
     let mut x = 1;
     let mut cycle = 0;
     let mut canvas = vec![vec!["-"; 40]; 6];
-    include_str!("input/day10.input")
-        .lines()
-        .map(|s| {
-            let mut splat = s.split_whitespace().skip(1);
-            if let Some(a) = splat.next() {
-                Instruction::AddX(a.parse().unwrap())
+    parse_ops().for_each(|i| {
+        let (adder, wait) = i.into();
+        for i in 0..wait {
+            let index_x = cycle % 40;
+            let index_y = cycle / 40;
+            cycle += 1;
+            canvas[index_y][index_x] = if index_x as i32 >= x - 1 && index_x as i32 <= x + 1 {
+                "#"
             } else {
-                Instruction::NoOp
-            }
-        })
-        .for_each(|i| {
-            let mut adder = 0;
-            let wait;
-            match i {
-                Instruction::NoOp => wait = 1,
-                Instruction::AddX(o) => {
-                    adder = o;
-                    wait = 2;
-                }
+                "."
             };
-            for i in 0..wait {
-                let index_x = cycle % 40;
-                let index_y = cycle / 40;
-                cycle += 1;
-                canvas[index_y][index_x] = if index_x as i32 >= x - 1 && index_x as i32 <= x + 1 {
-                    "#"
-                } else {
-                    "."
-                };
 
-                if i == wait - 1 {
-                    x += adder;
-                }
+            if i == wait - 1 {
+                x += adder;
             }
-        });
+        }
+    });
     canvas.into_iter().for_each(|r| {
         println!("{}", r.join(""));
     });
