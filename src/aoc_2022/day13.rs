f18dcea62da43161d2ea619782584efc347f1fe9
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-13 15:43:46 +0100

    Finished part 1

--- src/aoc_2022/day13.rs
+++ src/aoc_2022/day13.rs
@@ -2,7 +2,7 @@ use std::collections::VecDeque;
 
 use serde::Deserialize;
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Clone, PartialEq, Eq)]
 #[serde(untagged)]
 enum Data {
     List(VecDeque<Data>),
@@ -16,32 +16,7 @@ impl std::fmt::Debug for Data {
         }
     }
 }
-impl Data {
-    fn is_value(&self) -> bool {
-        match self {
-            Data::List(_) => false,
-            Data::Value(_) => true,
-        }
-    }
-    fn is_less(&self, x: &Data) -> bool {
-        match (self, x) {
-            (Data::Value(x), Data::Value(y)) => x < y,
-            (Data::List(l1), Data::List(l2)) => {
-                battle(Data::List(l1.clone()), Data::List(l2.clone()), 0)
-            }
-            _ => false,
-        }
-    }
-    fn is_same(&self, x: &Data) -> bool {
-        match (self, x) {
-            (Data::Value(x), Data::Value(y)) => x == y,
-            (Data::List(l1), Data::List(l2)) => {
-                battle(Data::List(l1.clone()), Data::List(l2.clone()), 0)
-            }
-            _ => false,
-        }
-    }
-}
+impl Data {}
 #[derive(Debug, Deserialize)]
 struct Pair {
     data1: Data,
@@ -59,91 +34,53 @@ pub fn part1() -> usize {
             serde_json::from_str::<Pair>(&str).unwrap()
         })
         .collect::<Vec<_>>();
+    let mut sum = 0;
     #[allow(clippy::never_loop)]
     for (i, Pair { data1, data2 }) in input.iter().enumerate() {
-        println!("{}: {}", i + 1, battle(data1.clone(), data2.clone(), 0));
-        println!()
+        //println!("{data1:?}\n{data2:?}");
+        let i = i + 1;
+        let res = battle(data1.clone(), data2.clone());
+        println!("{i}: {res:?}");
+        if let Status::Ok = res {
+            sum += i
+        }
     }
-    input
-        .into_iter()
-        .enumerate()
-        .filter(|(_, Pair { data1, data2 })| battle(data1.clone(), data2.clone(), 0))
-        .map(|(i, _)| i + 1)
-        .sum()
-    //0
+    sum
 }
-fn battle(data1: Data, data2: Data, indent: usize) -> bool {
-    let i = "\t".repeat(indent + 1);
+#[derive(Debug, PartialEq, Eq)]
+enum Status {
+    Ok,
+    Continue,
+    Bad,
+}
+fn battle(data1: Data, data2: Data) -> Status {
     match (data1, data2) {
-        (Data::List(mut v1), Data::List(mut v2)) if v1.len() == 1 && v1[0].is_value() => {
-            println!("{i}list[int] list: {v1:?} {v2:?}");
-            let val = v1.pop_front().unwrap();
-            while let Some(right) = v2.pop_front() {
-                println!("{i}int int: {val:?} {right:?}");
-                if val.is_less(&right) {
-                    return true;
-                } else if val.is_same(&right) {
-                    continue;
-                } else {
-                    return false;
+        (Data::List(l), Data::List(r)) if l.is_empty() && r.is_empty() => Status::Continue,
+        (Data::List(mut l), Data::List(mut r)) => {
+            while !r.is_empty() && !l.is_empty() {
+                let left = l.pop_front().unwrap();
+                let right = r.pop_front().unwrap();
+                let res = battle(left, right);
+
+                if res != Status::Continue {
+                    return res;
                 }
             }
-            true
-        }
-        (Data::List(mut v1), Data::List(mut v2)) if v2.len() == 1 && v2[0].is_value() => {
-            println!("{i}list list[int]: {v1:?} {v2:?}");
-            let val = v2.pop_front().unwrap();
-            while let Some(left) = v1.pop_front() {
-                println!("{i}int int: {left:?} {val:?}");
-                if left.is_less(&val) {
-                    return true;
-                } else if left.is_same(&val) {
-                    continue;
-                } else {
-                    return false;
-                }
+            if !l.is_empty() {
+                Status::Bad
+            } else if l.is_empty() && r.is_empty() {
+                Status::Continue
+            } else {
+                Status::Ok
             }
-            true
-        }
-        (Data::List(mut v1), Data::List(mut v2)) => {
-            println!("{i}list list: {v1:?} {v2:?}");
-            while let Some(right) = v2.pop_front() {
-                if let Some(left) = v1.pop_front() {
-                    println!("{i}int int: {left:?} {right:?}");
-                    if left.is_less(&right) {
-                        return true;
-                    } else if left.is_same(&right) {
-                        continue;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return true;
-                }
-            }
-            !v2.is_empty() || v1.is_empty()
         }
 
-        (Data::List(l), Data::Value(x)) => {
-            println!("{i}list val: {l:?} {x:?}");
-            battle(
-                Data::List(l),
-                Data::List(vec![Data::Value(x)].into()),
-                indent + 1,
-            )
-        }
-        (Data::Value(x), Data::List(l)) => {
-            println!("{i}val list: {x:?} {l:?}");
-            battle(
-                Data::List(vec![Data::Value(x)].into()),
-                Data::List(l),
-                indent + 1,
-            )
-        }
-        _ => unreachable!(), //(Data::Value(left), Data::Value(right)) => {
-                             //    println!("{i}val val: {left:?} {right:?}");
-                             //    left < right
-                             //}
+        (Data::List(l), v) => battle(Data::List(l), Data::List(vec![v].into())),
+        (v, Data::List(l)) => battle(Data::List(vec![v].into()), Data::List(l)),
+
+        (Data::Value(l), Data::Value(r)) if l < r => Status::Ok,
+        (Data::Value(l), Data::Value(r)) if l == r => Status::Continue,
+        (Data::Value(_), Data::Value(_)) => Status::Bad,
     }
 }
 
