632d1c3f055adf936872c921507538489e110feb
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-15 23:02:02 +0100

    Vacuumed day 15.

--- src/aoc_2022/day15.rs
+++ src/aoc_2022/day15.rs
@@ -1,7 +1,3 @@
-use std::collections::{BTreeSet, HashSet};
-
-use num_bigint::BigInt;
-
 use crate::utils::{manhattan_distance, parse_next};
 #[derive(Debug, Clone, Copy)]
 struct Beacon {
@@ -35,10 +31,8 @@ fn create_line(a: (isize, isize), b: (isize, isize)) -> (isize, isize) {
     let m = b.1 - (d * b.0 as f64).round() as isize;
     (d as isize, m)
 }
-
-pub fn part1() -> usize {
-    const Y: isize = 2000000;
-    let bs = include_str!("input/day15.input")
+fn parse_input() -> Vec<(Sensor, Beacon)> {
+    include_str!("input/day15.input")
         .lines()
         .map(|r| {
             let mut splat = r
@@ -55,7 +49,12 @@ pub fn part1() -> usize {
                 },
             )
         })
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
+
+pub fn part1() -> usize {
+    const Y: isize = 2000000;
+    let bs = parse_input();
     let min_x = bs
         .iter()
         .map(|(s, b)| s.x.min(b.x) - manhattan_distance((s.x, s.y), (b.x, b.y)))
@@ -66,7 +65,6 @@ pub fn part1() -> usize {
         .map(|(s, b)| s.x.max(b.x) + manhattan_distance((s.x, s.y), (b.x, b.y)))
         .max()
         .unwrap();
-    println!("{min_x} {max_x}");
     let mut row = vec![Spot::Empty; (max_x - min_x) as usize];
 
     for (s, b) in bs.iter() {
@@ -78,13 +76,11 @@ pub fn part1() -> usize {
         }
     }
 
-    let b_len = bs.len() - 1;
     bs.into_iter()
-        .enumerate()
-        .map(|(i, (s, b))| (i, (s, b), manhattan_distance((s.x, s.y), (b.x, b.y))))
-        .filter(|(_, (s, _), size)| (Y <= s.y + size) && (Y >= s.y - size))
-        .map(|(i, (s, _), size)| (i, s, size))
-        .for_each(|(i, Sensor { x: s_x, y: s_y }, size)| {
+        .map(|(s, b)| ((s, b), manhattan_distance((s.x, s.y), (b.x, b.y))))
+        .filter(|((s, _), size)| (Y <= s.y + size) && (Y >= s.y - size))
+        .map(|((s, _), size)| (s, size))
+        .for_each(|(Sensor { x: s_x, y: s_y }, size)| {
             let is_top = Y < s_y;
             let (d, m) = create_line(
                 if is_top {
@@ -100,43 +96,19 @@ pub fn part1() -> usize {
             );
             let x = (Y - m) / d;
             let dif = (s_x - x).abs();
-            //println!("{dif} ");
             let range = s_x - dif..=s_x + dif;
-            //println!("{range:?}");
             for x in range {
                 if row[(x - min_x) as usize] == Spot::Empty {
                     row[(x - min_x) as usize] = Spot::Occupied
                 }
             }
-            //println!()
         });
 
-    //row.iter().for_each(|s| print!("{s:?}"));
-    //println!();
-    //println!("...................###S#############.###########.........");
     row.into_iter().filter(|s| *s == Spot::Occupied).count()
 }
 
-pub fn part2() -> BigInt {
-    let bs = include_str!("input/day15.input")
-        .lines()
-        .map(|r| {
-            let mut splat = r
-                .split(|c: char| c.is_alphabetic() || c == ',' || c == '=' || c == ':' || c == ' ')
-                .filter(|s| !s.is_empty());
-            (
-                Sensor {
-                    x: parse_next(&mut splat),
-                    y: parse_next(&mut splat),
-                },
-                Beacon {
-                    x: parse_next(&mut splat),
-                    y: parse_next(&mut splat),
-                },
-            )
-        })
-        .collect::<Vec<_>>();
-
+pub fn part2() -> isize {
+    let bs = parse_input();
     let max = 4000000;
     for y in 0..max {
         let mut x = 0;
@@ -149,14 +121,10 @@ pub fn part2() -> BigInt {
                 let dif = (y - s.1).abs();
                 x = s.0 + size - dif;
             } else {
-                println!("{x} {y}");
-                return BigInt::from(x) * BigInt::from(4000000) + BigInt::from(y);
+                return x * 4000000 + y;
             }
             x += 1;
         }
     }
-    //    let ans = BigInt::from(x) * BigInt::from(4000000) + BigInt::from(y);
-    i128::MIN.into()
-    //let (x, y) = points.pop_first().unwrap();
-    //BigInt::from(x) * BigInt::from(4000000) + BigInt::from(y)
+    isize::MIN
 }
