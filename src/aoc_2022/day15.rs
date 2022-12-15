048ceda3bfe25778da3baf68a7ccda9a92d18c40
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-15 20:22:23 +0100

    Almost :(

--- src/aoc_2022/day15.rs
+++ src/aoc_2022/day15.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashSet};
 
 use num_bigint::BigInt;
 
@@ -58,14 +58,15 @@ pub fn part1() -> usize {
         .collect::<Vec<_>>();
     let min_x = bs
         .iter()
-        .map(|(s, b)| s.x.min(b.x) - 2000000)
+        .map(|(s, b)| s.x.min(b.x) - manhattan_distance((s.x, s.y), (b.x, b.y)))
         .min()
         .unwrap();
     let max_x = bs
         .iter()
-        .map(|(s, b)| s.x.max(b.x) + 2000000)
+        .map(|(s, b)| s.x.max(b.x) + manhattan_distance((s.x, s.y), (b.x, b.y)))
         .max()
         .unwrap();
+    println!("{min_x} {max_x}");
     let mut row = vec![Spot::Empty; (max_x - min_x) as usize];
 
     for (s, b) in bs.iter() {
@@ -137,33 +138,60 @@ pub fn part2() -> BigInt {
         })
         .collect::<Vec<_>>();
 
-    let max = 20;
+    let max = 4000000;
+    let mut points = BTreeSet::new();
     let mut last = String::new();
-    for x in 0..max {
-        let p = format!("{:.3}", ((x as f64 / max as f64) * 100.0));
-        if p != last {
+    for (i, (s_x, s_y, _)) in bs
+        .iter()
+        .map(|(s, b)| ((s, b), manhattan_distance((s.x, s.y), (b.x, b.y))))
+        .map(|((Sensor { x, y }, _), size)| (*x, *y, size))
+        .enumerate()
+    {
+        let p = format!("{:.3?}", (i as f64 / (bs.len() - 1) as f64) * 100.0);
+        if last != p {
+            println!("{p}%");
             last = p;
-            println!("{last}%");
         }
-        for y in 0..max {
-            let mut contains = true;
-            for (Sensor { x: s_x, y: s_y }, size) in bs
-                .iter()
-                .map(|(s, b)| ((*s, *b), manhattan_distance((s.x, s.y), (b.x, b.y))))
-                .filter(|((s, _), size)| (y <= s.y + size) && (y >= s.y - size))
-                .map(|((s, _), size)| (s, size))
-            {
-                contains = contains && manhattan_distance((x, y), (s_x, s_y)) > size;
-                if !contains {
-                    break;
+        for (b_x, b_y, _) in bs
+            .iter()
+            .map(|(s, b)| ((s, b), manhattan_distance((s.x, s.y), (b.x, b.y))))
+            .map(|((Sensor { x, y }, _), size)| (*x, *y, size))
+        {
+            if s_x == b_x && s_y == b_y {
+                continue;
+            }
+            let (d, m) = create_line((s_x, s_y), (b_x, b_y));
+            'cringe: for x in s_x.min(b_x)..s_x.max(b_x) {
+                let y = d * x + m;
+                let mut clean = true;
+                for (c, size_c) in bs
+                    .iter()
+                    .map(|(s, b)| (s, manhattan_distance((s.x, s.y), (b.x, b.y))))
+                    .map(|(Sensor { x, y }, size)| ((*x, *y), size))
+                {
+                    clean = clean
+                        && x >= 0
+                        && x <= max
+                        && y >= 0
+                        && y <= max
+                        && manhattan_distance((x, y), c) > size_c;
+                    if !clean {
+                        continue 'cringe;
+                    }
+                }
+                if clean {
+                    points.insert((x, y));
                 }
             }
-            if contains {
-                return BigInt::from(x) * BigInt::from(4000000) + BigInt::from(y);
-            }
-            //println!();
         }
     }
-
-    BigInt::from(0)
+    println!("{points:?}");
+    points.into_iter().for_each(|x| {
+        let (x, y) = x;
+        let ans = BigInt::from(x) * BigInt::from(4000000) + BigInt::from(y);
+        println!("{ans:?}");
+    });
+    0.into()
+    //let (x, y) = points.pop_first().unwrap();
+    //BigInt::from(x) * BigInt::from(4000000) + BigInt::from(y)
 }
