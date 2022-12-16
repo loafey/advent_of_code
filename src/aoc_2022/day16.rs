ddf490ee5f3b61a5cf697db133c675328e8c197f
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-16 10:41:59 +0100

    Started working on day 16.

--- src/aoc_2022/day16.rs
+++ src/aoc_2022/day16.rs
@@ -1,4 +1,38 @@
+use std::collections::BTreeMap;
+
+#[derive(Debug, Clone)]
+struct Valve {
+    flow_rate: isize,
+    connections: Vec<&'static str>,
+}
+
 pub fn part1() -> i32 {
+    let map = include_str!("input/day16.input")
+        .lines()
+        .map(|s| {
+            let mut splat = s
+                .split(|c| c == '=' || c == ';' || c == ',' || c == ' ')
+                .filter(|s| !s.is_empty())
+                .skip(1);
+            let name = splat.next().unwrap();
+            for _ in 0..3 {
+                splat.next();
+            }
+            let flow_rate = splat.next().unwrap().parse::<isize>().unwrap();
+            for _ in 0..4 {
+                splat.next();
+            }
+            let connections = splat.collect::<Vec<_>>();
+            (
+                name,
+                Valve {
+                    flow_rate,
+                    connections,
+                },
+            )
+        })
+        .collect::<BTreeMap<_, _>>();
+    println!("{map:#?}");
     0
 }
 
