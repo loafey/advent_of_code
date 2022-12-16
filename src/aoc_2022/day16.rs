ef4e12a5923e2674cb85cf9de7f7008dfd0e0e43
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-16 11:30:56 +0100

    Did some more work.

--- src/aoc_2022/day16.rs
+++ src/aoc_2022/day16.rs
@@ -3,8 +3,10 @@ use std::collections::BTreeMap;
 #[derive(Debug, Clone)]
 struct Valve {
     flow_rate: isize,
-    connections: Vec<&'static str>,
+    connections: Vec<Str>,
 }
+type Map = BTreeMap<Str, Valve>;
+type Str = &'static str;
 
 pub fn part1() -> i32 {
     let map = include_str!("input/day16.input")
@@ -31,9 +33,43 @@ pub fn part1() -> i32 {
                 },
             )
         })
-        .collect::<BTreeMap<_, _>>();
-    map.into_iter().for_each(|s| println!("{s:?}"));
+        .collect::<Map>();
+    let mut current = "AA";
+    let mut released = map
+        .iter()
+        .filter(|(_, v)| v.flow_rate == 0)
+        .map(|(k, _)| k)
+        .collect::<Vec<_>>();
+    #[allow(clippy::never_loop)]
+    loop {
+        map.iter()
+            .filter(|(k, _)| !released.contains(k))
+            .for_each(|(k, v)| {
+                println!("{k:?} {}", v.flow_rate);
+            });
+        println!("{:?}", dfs("AA", "DD", &map));
+        break;
+    }
+    // loop time < 30:
+    //  Calculate best next option
+    //      cost to go there / preasure it will release
+    //  Path find there
     0
+}
+
+fn dfs(from: Str, target: Str, map: &Map) {
+    fn go(from: Str, target: Str, map: &Map, visited: &mut Vec<&str>) {
+        if from == target {
+        } else {
+            visited.push(from);
+            for n in &map[from].connections {
+                if !visited.contains(n) {
+                    go(n, target, map, visited)
+                }
+            }
+        }
+    }
+    go(from, target, map, &mut Vec::new())
 }
 
 pub fn part2() -> i32 {
