982ecada2c92559e76bd7a6f9b076138ea28f09c
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-16 23:53:16 +0100

    Finished part 1 @SebastianSelander

--- src/aoc_2022/day16.rs
+++ src/aoc_2022/day16.rs
@@ -1,13 +1,14 @@
 use std::collections::BTreeMap;
 
-use pathfinding::prelude::dijkstra;
+use memoize::memoize;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 struct Valve {
     flow_rate: isize,
-    connections: Vec<(Str, isize)>,
+    connections: Vec<Str>,
 }
 type Map = BTreeMap<Str, Valve>;
+
 type Str = &'static str;
 
 pub fn part1() -> isize {
@@ -26,7 +27,7 @@ pub fn part1() -> isize {
             for _ in 0..4 {
                 splat.next();
             }
-            let connections = splat.map(|s| (s, 1)).collect::<Vec<_>>();
+            let connections = splat.collect::<Vec<_>>();
             (
                 name,
                 Valve {
@@ -36,73 +37,35 @@ pub fn part1() -> isize {
             )
         })
         .collect::<Map>();
-    let mut current = "AA";
-    print!("AA ");
-    let mut count = 1;
-    let mut flow = 0;
-    let mut released = map
-        .iter()
-        .filter(|(_, v)| v.flow_rate == 0)
-        .map(|(k, _)| *k)
-        .collect::<Vec<_>>();
 
-    while count < 30 {
-        if let Some((target, _, path, _, _)) = map
-            .iter()
-            .filter(|(k, _)| !released.contains(k))
-            .map(|(k, v)| {
-                let (path, cost) = pathfind(current, k, &map);
-                //println!("{k:?} {}", cost as f64 / v.flow_rate as f64);
-                let prio = (cost as f64).powi(2) / (v.flow_rate) as f64;
-                //println!("\t{k} {prio}");
-                (k, v, path, cost, prio)
-            })
-            .min_by(|(_, _, _, _, c1), (_, _, _, _, c2)| c1.total_cmp(c2))
-        {
-            for _p in path {
-                count += 1;
-                flow += released.iter().map(|s| map[s].flow_rate).sum::<isize>();
-                print!("{_p} ");
-            }
-            //println!("{target} :{count}");
-            current = target;
-            released.push(current);
-        }
-        count += 1;
-        // öppna DD 2, BB 5, JJ 9, HH 17, EE 21, CC 24
-        // AA DD CC BB AA II JJ II AA DD EE FF GG HH GG FF EE DD CC
-        // AA DD CC BB AA II JJ II AA DD EE FF GG HH GG FF EE DD CC
-
-        flow += released.iter().map(|s| map[s].flow_rate).sum::<isize>();
-    }
-    // loop time < 30:
-    //  Calculate best next option
-    //      cost to go there / preasure it will release
-    //  Path find there
-    flow
+    rinzal_dp(map.clone(), "AA", 29)
 }
 
-fn pathfind(from: Str, target: Str, map: &Map) -> (Vec<Str>, isize) {
-    //fn go(from: Str, target: Str, map: &Map, visited: &mut Vec<&str>, done: &mut bool) -> Vec<Str> {
-    //    let mut buf = vec![from];
-    //
-    //    for n in &map[from].connections {
-    //        if from == target {
-    //            *done = true;
-    //            visited.push(n);
-    //            break;
-    //        } else if !visited.contains(n) && !*done {
-    //            visited.push(n);
-    //            buf.append(&mut go(n, target, map, visited, done));
-    //        }
-    //    }
-    //    buf
-    //}
-    //go(from, target, map, &mut Vec::new(), &mut false)
-    let (mut path, cost) =
-        dijkstra(&from, |p| map[p].connections.clone(), |p| p == &target).unwrap();
-    path.remove(0);
-    (path, cost)
+#[memoize]
+fn rinzal_dp(map: Map, current: Str, mins: isize) -> isize {
+    match mins {
+        0 => 0,
+        _ => {
+            let current_flow_rate = map[current].flow_rate;
+            let open = (mins * current_flow_rate)
+                + rinzal_dp(
+                    {
+                        let mut map = map.clone();
+                        map.get_mut(current).unwrap().flow_rate = 0;
+                        map
+                    },
+                    current,
+                    mins - 1,
+                );
+            let mov = map[current]
+                .connections
+                .iter()
+                .map(|x| rinzal_dp(map.clone(), x, mins - 1))
+                .max()
+                .unwrap();
+            mov.max(open)
+        }
+    }
 }
 
 pub fn part2() -> i32 {
