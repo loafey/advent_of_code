32e2086cff3738d8af9f199bfd9c7a8d1c33b3bc
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-18 00:25:59 +0100

    Started work on day 17.

--- src/aoc_2022/day17.rs
+++ src/aoc_2022/day17.rs
@@ -15,7 +15,7 @@ impl std::fmt::Debug for Spot {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Rock => write!(f, "██"),
-            Self::Empty => write!(f, "░░"),
+            Self::Empty => write!(f, ".."), //░░
         }
     }
 }
@@ -34,11 +34,11 @@ enum Rocks {
 impl Rocks {
     fn next(self) -> Self {
         match self {
-            Rocks::First => Rocks::Fifth,
-            Rocks::Second => Rocks::First,
-            Rocks::Third => Rocks::Second,
-            Rocks::Fourth => Rocks::Third,
-            Rocks::Fifth => Rocks::Fourth,
+            Rocks::First => Rocks::Second,
+            Rocks::Second => Rocks::Third,
+            Rocks::Third => Rocks::Fourth,
+            Rocks::Fourth => Rocks::Fifth,
+            Rocks::Fifth => Rocks::First,
         }
     }
     fn into_arr(self) -> Box<[Box<[Spot]>]> {
@@ -71,46 +71,47 @@ pub fn part1() -> i32 {
         })
         .collect::<Vec<_>>();
 
-    let mut grid: VecDeque<[Spot; 7]> = vec![[E, E, E, E, E, E, E]; 7].into();
+    let mut grid: VecDeque<[Spot; 7]> = vec![[E, E, E, E, E, E, E]; 4].into();
     let mut current = Rocks::First;
     let mut coords = [2, 0];
     for m in input {
         let arr = current.into_arr();
-        println!("\n╔══════════════╗");
-        grid.iter().enumerate().for_each(|(y, r)| {
-            print!("║");
-            r.iter().enumerate().for_each(|(x, r)| {
-                if y >= coords[1]
-                    && y < coords[1] + arr.len()
-                    && x >= coords[0]
-                    && x < coords[0] + arr[0].len()
-                    && arr[y - coords[1]][x - coords[0]] != Spot::Empty
-                {
-                    print!("{:?}", arr[y - coords[1]][x - coords[0]])
-                } else {
-                    print!("{r:?}");
+        print_grid(&grid, coords, &arr);
+        match m {
+            Move::Left => {
+                if coords[0] > 0 {
+                    coords[0] -= 1
                 }
-            });
-            println!("║")
-        });
-        println!("╚══════════════╝");
+            }
+
+            Move::Right => {
+                if coords[0] + arr[0].len() < grid[0].len() {
+                    coords[0] += 1
+                }
+            }
+        }
+        print_grid(&grid, coords, &arr);
 
         coords[1] += 1;
         let colliding = {
-            let mut col = false;
+            let mut cols = 0;
             for (y, r) in arr.iter().enumerate() {
+                let mut y_done = false;
                 for (x, c) in r.iter().enumerate() {
-                    if *c != Spot::Empty {
-                        col = col || grid[coords[1] + y][coords[0] + x] == Spot::Rock;
+                    if *c != Spot::Empty
+                        && grid[coords[1] + y][coords[0] + x] == Spot::Rock
+                        && !y_done
+                    {
+                        cols += 1;
+                        y_done = true;
                     }
                 }
             }
-            col
+            cols
         };
-        if colliding {
-            coords[1] -= 1
-        }
-        if coords[1] + arr.len() >= grid.len() || colliding {
+        println!("{}", colliding);
+        coords[1] -= colliding;
+        if coords[1] + arr.len() >= grid.len() || colliding > 0 {
             for (y, r) in arr.iter().enumerate() {
                 for (x, c) in r.iter().enumerate() {
                     if *c != Spot::Empty {
@@ -119,20 +120,41 @@ pub fn part1() -> i32 {
                 }
             }
             let dif = grid.len() - (grid.len() - coords[1]);
+            coords = [2, 0];
+            current = current.next();
             if dif <= 3 {
-                for _ in 0..3 - dif {
+                for _ in 0..(3 - dif) + current.into_arr().len() {
                     grid.push_front([Spot::Empty; 7])
                 }
             }
-            coords = [2, 0];
-            current = current.next();
         }
 
-        std::thread::sleep_ms(400)
+        std::thread::sleep_ms(800)
     }
     0
 }
 
 pub fn part2() -> i32 {
     0
+}
+
+fn print_grid(grid: &VecDeque<[Spot; 7]>, coords: [usize; 2], arr: &[Box<[Spot]>]) {
+    println!("\n╔══════════════╗");
+    grid.iter().enumerate().for_each(|(y, r)| {
+        print!("║");
+        r.iter().enumerate().for_each(|(x, r)| {
+            if y >= coords[1]
+                && y < coords[1] + arr.len()
+                && x >= coords[0]
+                && x < coords[0] + arr[0].len()
+                && arr[y - coords[1]][x - coords[0]] != Spot::Empty
+            {
+                print!("{:?}", arr[y - coords[1]][x - coords[0]])
+            } else {
+                print!("{r:?}");
+            }
+        });
+        println!("║")
+    });
+    println!("╚══════════════╝");
 }
