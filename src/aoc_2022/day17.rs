60cc1204c175e1191cfbf5d97948be38b61b262d
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2022-12-19 23:13:01 +0100

    Finished day 17 part 1.

--- src/aoc_2022/day17.rs
+++ src/aoc_2022/day17.rs
@@ -14,8 +14,10 @@ enum Spot {
 impl std::fmt::Debug for Spot {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Rock => write!(f, "██"),
-            Self::Empty => write!(f, ".."), //░░
+            Self::Rock => write!(f, "#"),
+            Self::Empty => write!(f, "."),
+            //Self::Rock => write!(f, "██"),
+            //Self::Empty => write!(f, ".."), //░░
         }
     }
 }
@@ -59,8 +61,32 @@ impl Rocks {
         }
     }
 }
+fn check_colliding(
+    dir: [isize; 2],
+    coords: [usize; 2],
+    box_arr: &[Box<[Spot]>],
+    grid: &VecDeque<[Spot; 7]>,
+) -> bool {
+    let colliding = {
+        let mut cols = false;
+        'brum: for (y, r) in box_arr.iter().enumerate() {
+            for (x, c) in r.iter().enumerate() {
+                if *c != Spot::Empty
+                    && grid[(coords[1] as isize + y as isize + dir[1]) as usize]
+                        [(coords[0] as isize + x as isize + dir[0]) as usize]
+                        == Spot::Rock
+                {
+                    cols = true;
+                    break 'brum;
+                }
+            }
+        }
+        cols
+    };
+    colliding
+}
 
-pub fn part1() -> i32 {
+pub fn part1() -> usize {
     let input = include_str!("input/day17.input")
         .chars()
         .filter(|c| !c.is_whitespace())
@@ -74,44 +100,35 @@ pub fn part1() -> i32 {
     let mut grid: VecDeque<[Spot; 7]> = vec![[E, E, E, E, E, E, E]; 4].into();
     let mut current = Rocks::First;
     let mut coords = [2, 0];
-    for m in input {
+    let mut rock_count = 1;
+    for m in input.iter().cycle() {
         let arr = current.into_arr();
-        print_grid(&grid, coords, &arr);
         match m {
             Move::Left => {
-                if coords[0] > 0 {
+                if coords[0] > 0 && !check_colliding([-1, 0], coords, &arr, &grid) {
                     coords[0] -= 1
                 }
             }
 
             Move::Right => {
-                if coords[0] + arr[0].len() < grid[0].len() {
+                if coords[0] + arr[0].len() < grid[0].len()
+                    && !check_colliding([1, 0], coords, &arr, &grid)
+                {
                     coords[0] += 1
                 }
             }
         }
-        print_grid(&grid, coords, &arr);
+        //print_grid(&grid, coords, &arr);
 
-        coords[1] += 1;
-        let colliding = {
-            let mut cols = 0;
-            for (y, r) in arr.iter().enumerate() {
-                let mut y_done = false;
-                for (x, c) in r.iter().enumerate() {
-                    if *c != Spot::Empty
-                        && grid[coords[1] + y][coords[0] + x] == Spot::Rock
-                        && !y_done
-                    {
-                        cols += 1;
-                        y_done = true;
-                    }
-                }
+        let colliding =
+            coords[1] + arr.len() >= grid.len() || check_colliding([0, 1], coords, &arr, &grid);
+        if !colliding {
+            coords[1] += 1;
+        }
+        if coords[1] + arr.len() > grid.len() || colliding {
+            if coords[1] + arr.len() > grid.len() {
+                coords[1] -= 1;
             }
-            cols
-        };
-        println!("{}", colliding);
-        coords[1] -= colliding;
-        if coords[1] + arr.len() >= grid.len() || colliding > 0 {
             for (y, r) in arr.iter().enumerate() {
                 for (x, c) in r.iter().enumerate() {
                     if *c != Spot::Empty {
@@ -119,18 +136,42 @@ pub fn part1() -> i32 {
                     }
                 }
             }
-            let dif = grid.len() - (grid.len() - coords[1]);
             coords = [2, 0];
             current = current.next();
-            if dif <= 3 {
-                for _ in 0..(3 - dif) + current.into_arr().len() {
-                    grid.push_front([Spot::Empty; 7])
+            rock_count += 1;
+
+            if let Some((height, _)) = grid
+                .iter()
+                .enumerate()
+                .find(|(_, r)| r.iter().filter(|s| **s != Spot::Empty).count() > 0)
+            {
+                let arr = current.into_arr();
+                // println!("{} {}", height, arr.len());
+                if arr.len() + 3 >= height {
+                    for _ in 0..(arr.len() + 3 - height) {
+                        grid.push_front([E, E, E, E, E, E, E])
+                    }
                 }
+                if height > arr.len() + 3 {
+                    coords[1] += height - arr.len() - 3;
+                }
+            }
+
+            if rock_count >= 2023 {
+                print_grid(&grid, coords, &[]);
+                return grid.len()
+                    - grid
+                        .iter()
+                        .enumerate()
+                        .find(|(_, r)| r.iter().filter(|s| **s != Spot::Empty).count() > 0)
+                        .unwrap()
+                        .0;
             }
         }
 
-        std::thread::sleep_ms(800)
+        //std::thread::sleep_ms(100)
     }
+    //print_grid(&grid, coords, &[Box::new([])]);
     0
 }
 
@@ -139,9 +180,10 @@ pub fn part2() -> i32 {
 }
 
 fn print_grid(grid: &VecDeque<[Spot; 7]>, coords: [usize; 2], arr: &[Box<[Spot]>]) {
-    println!("\n╔══════════════╗");
+    //println!("\n╔══════════════╗");
     grid.iter().enumerate().for_each(|(y, r)| {
-        print!("║");
+        //print!("║");
+        print!("|");
         r.iter().enumerate().for_each(|(x, r)| {
             if y >= coords[1]
                 && y < coords[1] + arr.len()
@@ -154,7 +196,9 @@ fn print_grid(grid: &VecDeque<[Spot; 7]>, coords: [usize; 2], arr: &[Box<[Spot]>
                 print!("{r:?}");
             }
         });
-        println!("║")
+        //println!("║")
+        println!("|")
     });
-    println!("╚══════════════╝");
+    //println!("╚══════════════╝");
+    println!("+-------+");
 }
