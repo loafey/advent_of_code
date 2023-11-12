7a8d643cac2e225c179a29fc8d4bb85985cdead0
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-11-12 20:27:08 +0100

    Crumping.

---
+++ src/aoc_2019/day4.rs
@@ -0,0 +1,39 @@
+fn split_num(i: usize) -> [usize; 6] {
+    [
+        (i / 100000) % 10,
+        (i / 10000) % 10,
+        (i / 1000) % 10,
+        (i / 100) % 10,
+        (i / 10) % 10,
+        i % 10,
+    ]
+}
+fn meets_requirements(i: usize) -> bool {
+    fn has_adjacents(i: [usize; 6]) -> bool {
+        for ind in 0..i.len() - 1 {
+            if i[ind] == i[ind + 1] {
+                return true;
+            }
+        }
+        false
+    }
+    fn no_decrease(i: [usize; 6]) -> bool {
+        for ind in 0..i.len() - 1 {
+            if i[ind] > i[ind + 1] {
+                return false;
+            }
+        }
+        true
+    }
+    let i = split_num(i);
+
+    has_adjacents(i) && no_decrease(i)
+}
+
+pub fn part1() -> usize {
+    (172930..683082).filter(|p| meets_requirements(*p)).count()
+}
+
+pub fn part2() -> i32 {
+    0
+}
