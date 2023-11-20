a81749e180dc7e63ed13eded393d798c0811a132
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-11-20 19:42:21 +0100

    Did a bunch of days on 2020

---
+++ src/aoc_2020/day1.rs
@@ -0,0 +1,32 @@
+use crate::utils::load_string;
+
+pub fn part1() -> i64 {
+    let s = load_string("src/aoc_2020/day1.input")
+        .lines()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    for x in &s {
+        for y in &s {
+            if *x + *y == 2020 {
+                return *x * *y;
+            }
+        }
+    }
+    0
+}
+pub fn part2() -> i64 {
+    let s = load_string("src/aoc_2020/day1.input")
+        .lines()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    for x in &s {
+        for y in &s {
+            for z in &s {
+                if *x + *y + *z == 2020 {
+                    return *x * *y * *z;
+                }
+            }
+        }
+    }
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![feature(pattern)]
+#![feature(iter_array_chunks)]
+#![feature(let_chains)]
 //mod aoc_2018;
 //mod aoc_2022;
 mod aoc_2019;
+mod aoc_2020;
 mod aoc_2023;
 mod parser;
 mod utils;
 
 fn main() {
-    aoc_2023::table().run_day(1);
+    aoc_2020::table().run_day(3);
 }
