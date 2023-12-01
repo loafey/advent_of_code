a1de86b35223d2046a883212f9b977ef9d3b2950
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-12-01 12:04:10 +0100

    Speed up day 1

--- src/aoc_2023/day1.rs
+++ src/aoc_2023/day1.rs
@@ -1,34 +1,31 @@
 use crate::utils::load_string;
 
+fn solver(s: &str) -> i64 {
+    let mut s = s.chars().filter(|c| c.is_ascii_digit());
+    let first = s.next().unwrap();
+    let next = if let Some(c) = s.last() { c } else { first };
+    format!("{first}{next}").parse::<i64>().unwrap()
+}
+
 pub fn part1() -> i64 {
     load_string("inputs/2023/day1.input")
         .lines()
-        .map(|s| {
-            let mut s = s.chars().filter(|c| c.is_ascii_digit());
-            let first = s.next().unwrap();
-            let next = if let Some(c) = s.last() { c } else { first };
-            format!("{first}{next}").parse::<i64>().unwrap()
-        })
+        .map(solver)
         .sum()
 }
 
 pub fn part2() -> i64 {
     load_string("inputs/2023/day1.input")
-        .replace("two", "two2two")
-        .replace("one", "one1one")
-        .replace("three", "three3three")
-        .replace("four", "four4four")
-        .replace("five", "five5five")
-        .replace("six", "six6six")
-        .replace("seven", "seven7seven")
-        .replace("eight", "eight8eight")
-        .replace("nine", "nine9nine")
+        .replace("two", "t2o")
+        .replace("one", "o1e")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
         .lines()
-        .map(|s| {
-            let mut s = s.chars().filter(|c| c.is_ascii_digit());
-            let first = s.next().unwrap();
-            let next = if let Some(c) = s.last() { c } else { first };
-            format!("{first}{next}").parse::<i64>().unwrap()
-        })
+        .map(solver)
         .sum()
 }
