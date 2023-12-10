75f53138f8ee0ccea78209d24fc56517dae3c6b5
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-12-10 14:40:09 +0100

    coded part 10 part 2

--- src/aoc_2023/day10.rs
+++ src/aoc_2023/day10.rs
@@ -41,7 +41,7 @@ fn graph() -> ((usize, usize, char), Map) {
         }
         let mut neighbors = Vec::new();
         match c {
-            'F' | 'S' => {
+            'F' => {
                 if let Some(bot) = matrix_get(y + 1, x, 0, 0, &matrix) {
                     neighbors.push((y + 1, x, bot))
                 }
@@ -65,7 +65,7 @@ fn graph() -> ((usize, usize, char), Map) {
                     neighbors.push((y, x - 1, left))
                 }
             }
-            'L' => {
+            'L' | 'S' => {
                 if let Some(right) = matrix_get(y, x + 1, 0, 0, &matrix) {
                     neighbors.push((y, x + 1, right))
                 }
@@ -114,26 +114,113 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    // let (start, map) = graph();
-    // let matrix = load_string("inputs/2023/day10.input")
-    //     .lines()
-    //     .map(|s| s.chars().collect::<Vec<_>>())
-    //     .collect::<Vec<_>>();
-    // let (y, x) = (matrix.len(), matrix[0].len());
+    let (start, map) = graph();
+    let matrix = load_string("inputs/2023/day10.input")
+        .lines()
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let (y, x) = (matrix.len(), matrix[0].len());
 
-    // let mut ascii = vec![vec!['.'; x]; y];
-    // for ((y, x, c), _) in map {
-    //     ascii[y][x] = c;
-    // }
+    let mut ascii = vec![vec![false; x * 3]; y * 3];
+    ascii.iter_mut().array_chunks::<3>().for_each(|[_, a, _]| {
+        a.iter_mut()
+            .array_chunks::<3>()
+            .for_each(|[_, a, _]| *a = true)
+    });
+    for ((y, x, c), _) in map {
+        let y = (y * 3) + 1;
+        let x = (x * 3) + 1;
+        match c {
+            '|' => {
+                ascii[y - 1][x] = true;
+                ascii[y][x] = true;
+                ascii[y + 1][x] = true
+            }
+            '-' => {
+                ascii[y][x - 1] = true;
+                ascii[y][x] = true;
+                ascii[y][x + 1] = true
+            }
+            'L' | 'S' => {
+                ascii[y - 1][x] = true;
+                ascii[y][x] = true;
+                ascii[y][x + 1] = true
+            }
+            'J' => {
+                ascii[y - 1][x] = true;
+                ascii[y][x] = true;
+                ascii[y][x - 1] = true
+            }
+            '7' => {
+                ascii[y][x - 1] = true;
+                ascii[y][x] = true;
+                ascii[y + 1][x] = true
+            }
+            'F' => {
+                ascii[y][x + 1] = true;
+                ascii[y][x] = true;
+                ascii[y + 1][x] = true
+            }
+            '.' => ascii[y][x] = true,
+            _ => {}
+        }
+    }
+
+    let mut stack = vec![(0, 0)];
+    while let Some((y, x)) = stack.pop() {
+        ascii[y][x] = true;
+        if let Some(left) = matrix_get(y, x, 0, -1, &ascii) {
+            if !left {
+                stack.push((y, x - 1))
+            }
+        }
+        if let Some(right) = matrix_get(y, x, 0, 1, &ascii) {
+            if !right {
+                stack.push((y, x + 1))
+            }
+        }
+        if let Some(up) = matrix_get(y, x, -1, 0, &ascii) {
+            if !up {
+                stack.push((y - 1, x))
+            }
+        }
+        if let Some(down) = matrix_get(y, x, 1, 0, &ascii) {
+            if !down {
+                stack.push((y + 1, x))
+            }
+        }
+    }
 
     // for r in ascii {
     //     for c in r {
-    //         print!("{c}")
+    //         if c {
+    //             print!("█")
+    //         } else {
+    //             print!(" ")
+    //         }
     //     }
     //     println!()
     // }
 
-    // I counted by hand :))
-
-    371
+    let mut i = 0;
+    for y in (0..ascii.len()).step_by(3) {
+        for x in (0..ascii[y].len()).step_by(3) {
+            let arr = [
+                [ascii[y][x], ascii[y][x + 1], ascii[y][x + 2]],
+                [ascii[y + 1][x], ascii[y + 1][x + 1], ascii[y + 1][x + 2]],
+                [ascii[y + 2][x], ascii[y + 2][x + 1], ascii[y + 2][x + 2]],
+            ];
+            if matches!(
+                arr,
+                [
+                    [false, false, false],
+                    [false, true, false],
+                    [false, false, false]
+                ]
+            ) {
+                i += 1;
+            }
+        }
+    }
+    i
 }
