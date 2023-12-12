d1c67e06ca42c7dc159725a29abfdb17729175b7
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-12-12 22:50:43 +0100

    my barlo ass could not be a beekeeper

--- src/aoc_2023/day12.rs
+++ src/aoc_2023/day12.rs
@@ -1,23 +1,35 @@
 use crate::utils::load_string;
 
-fn check(chars: &mut [char], nums: &mut [usize]) -> usize {
-    // println!("{chars:?} {nums:?}",);
-    match (chars, nums) {
-        (s, []) if s.is_empty() || s.iter().all(|c| *c == '.') => 1,
+#[memoize::memoize]
+fn check(chars: Vec<char>, nums: Vec<usize>) -> usize {
+    match (&chars[..], &nums[..]) {
+        (s, []) if s.is_empty() || s.iter().all(|c| matches!(c, '.' | '?')) => 1,
         (_, []) => 0,
         ([], _) => 0,
-        (css, [0, ns @ ..]) => check(css, ns),
+        (css, [0, ns @ ..]) => check(css.to_vec(), ns.to_vec()),
         (css, nss) => match css[0] {
             '#' => {
-                nss[0] -= 1;
-                check(&mut css[1..], nss)
+                let mut nums = nums.to_vec();
+                if nums[0] < css.len()
+                    && css[..nums[0]].iter().all(|c| matches!(c, '#' | '?'))
+                    && css[nums[0]] != '#'
+                {
+                    return check(css[nums[0] + 1..].to_vec(), nums[1..].to_vec());
+                } else if nums[0] == css.len()
+                    && nums.len() == 1
+                    && css.iter().all(|c| matches!(c, '#' | '?'))
+                {
+                    return 1;
+                }
+                0
             }
-            '.' => check(&mut css[1..], nss),
+            '.' => check(css[1..].to_vec(), nss.to_vec()),
             '?' => {
+                let mut css = css.to_vec();
                 css[0] = '#';
-                let a = check(css, nss);
+                let a = check(css.to_vec(), nss.to_vec());
                 css[0] = '.';
-                let b = check(css, nss);
+                let b = check(css.to_vec(), nss.to_vec());
                 a + b
             }
             _ => unreachable!(),
@@ -41,40 +53,30 @@ pub fn part1() -> usize {
         })
         .collect::<Vec<_>>();
 
-    inp.into_iter()
-        .map(|(mut nums, mut row)| {
-            print!("{row:?} {nums:?}");
-            let r = check(&mut row, &mut nums);
-            println!(": {r}");
-            r
-        })
-        .sum()
+    inp.into_iter().map(|(nums, row)| check(row, nums)).sum()
 }
 
 pub fn part2() -> usize {
-    0
-    // let binding = load_string("inputs/2023/day12.input");
-    // let inp = binding
-    //     .lines()
-    //     .map(|r| {
-    //         let (row, nums) = r.split_once(' ').unwrap();
-    //         let row = row.to_string();
-    //         let mut nums = nums
-    //             .split(',')
-    //             .map(|s| s.parse::<usize>().unwrap())
-    //             .collect::<Vec<_>>();
-    //         let mut row = row.chars().collect::<Vec<_>>();
-    //         let num_clone = nums.clone();
-    //         let row_clone = row.clone();
-    //         for _ in 0..4 {
-    //             row.push('?');
-    //             row.append(&mut row_clone.clone());
-    //             nums.append(&mut num_clone.clone());
-    //         }
-    //         (nums, row)
-    //     })
-    //     .collect::<Vec<_>>();
-    // inp.into_par_iter()
-    //     .map(|(nums, row)| perm(row, &nums))
-    //     .sum()
+    let binding = load_string("inputs/2023/day12.input");
+    let inp = binding
+        .lines()
+        .map(|r| {
+            let (row, nums) = r.split_once(' ').unwrap();
+            let row = row.to_string();
+            let mut nums = nums
+                .split(',')
+                .map(|s| s.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            let mut row = row.chars().collect::<Vec<_>>();
+            let num_clone = nums.clone();
+            let row_clone = row.clone();
+            for _ in 0..4 {
+                row.push('?');
+                row.append(&mut row_clone.clone());
+                nums.append(&mut num_clone.clone());
+            }
+            (nums, row)
+        })
+        .collect::<Vec<_>>();
+    inp.into_iter().map(|(nums, row)| check(row, nums)).sum()
 }
