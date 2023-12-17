4816c6264a246cb814c92e5e2a19725c215cbd6b
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-12-17 12:52:51 +0100

    finished day 17

--- src/aoc_2023/day17.rs
+++ src/aoc_2023/day17.rs
@@ -1,7 +1,4 @@
-use std::collections::BTreeSet;
-
 use crate::utils::{load_matrix_then, MatrixGet};
-use memoize::memoize;
 
 thread_local! {
     static MAP: Vec<Vec<usize>> = load_matrix_then("inputs/2023/day17.input", |c| c.to_digit(10).unwrap() as usize);
@@ -9,8 +6,32 @@ thread_local! {
 fn is_corner(y: usize, x: usize) -> bool {
     MAP.with(|map| y == map.len() - 1 && x == map[0].len() - 1)
 }
-fn get(y: usize, x: usize) -> Option<usize> {
-    MAP.with(|map| map.matrix_get(y, x, 0, 0).copied())
+fn get(y: usize, x: usize, ymod: isize, xmod: isize) -> Option<usize> {
+    MAP.with(|map| map.matrix_get(y, x, ymod, xmod).copied())
+}
+fn get_neighbors(
+    y: usize,
+    x: usize,
+    dir: Direction,
+    dir_amount: usize,
+) -> Vec<((usize, usize, Direction, usize), usize)> {
+    let (up_amount, down_amount, left_amount, right_amount) = match dir {
+        Up => (dir_amount + 1, 0, 0, 0),
+        Down => (0, dir_amount + 1, 0, 0),
+        Left => (0, 0, dir_amount + 1, 0),
+        Right => (0, 0, 0, dir_amount + 1),
+    };
+
+    [
+        get(y, x, -1, 0).map(|v| ((y - 1, x, Up, up_amount), v)),
+        get(y, x, 1, 0).map(|v| ((y + 1, x, Down, down_amount), v)),
+        get(y, x, 0, -1).map(|v| ((y, x - 1, Left, left_amount), v)),
+        get(y, x, 0, 1).map(|v| ((y, x + 1, Right, right_amount), v)),
+    ]
+    .into_iter()
+    .flatten()
+    .filter(|((_, _, n_dir, d), _)| n_dir.not_opposite(dir) && *d < 3)
+    .collect()
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -20,78 +41,71 @@ enum Direction {
     Up,
     Down,
 }
+use pathfinding::directed::dijkstra::dijkstra;
 use Direction::*;
-#[derive(Eq, Clone)]
-struct VisitedStack(BTreeSet<(usize, usize)>);
-
-impl std::hash::Hash for VisitedStack {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        0.hash(state);
-    }
-}
-
-impl PartialEq for VisitedStack {
-    fn eq(&self, _: &Self) -> bool {
-        true
-    }
-}
-
-#[memoize]
-fn solve_part1(
-    y: usize,
-    x: usize,
-    dir: Direction,
-    dir_amount: usize,
-    visited: VisitedStack,
-) -> usize {
-    let mut visited = visited;
-    if visited.0.contains(&(y, x)) || dir_amount > 3 {
-        10000
-    } else if is_corner(y, x) {
-        get(y, x).unwrap()
-    } else if let Some(a) = get(y, x) {
-        let (up_amount, down_amount, left_amount, right_amount) = match dir {
-            Up => (dir_amount + 1, 0, 0, 0),
-            Down => (0, dir_amount + 1, 0, 0),
-            Left => (0, 0, dir_amount + 1, 0),
-            Right => (0, 0, 0, dir_amount + 1),
-        };
-        visited.0.insert((y, x));
-        a + match dir {
-            Left => [
-                solve_part1(y + 1, x, Up, down_amount, visited.clone()),
-                solve_part1(y - 1, x, Down, up_amount, visited.clone()),
-                solve_part1(y, x - 1, Left, left_amount, visited.clone()),
-            ],
-            Right => [
-                solve_part1(y + 1, x, Up, down_amount, visited.clone()),
-                solve_part1(y - 1, x, Down, up_amount, visited.clone()),
-                solve_part1(y, x + 1, Right, right_amount, visited.clone()),
-            ],
-            Up => [
-                solve_part1(y + 1, x, Up, down_amount, visited.clone()),
-                solve_part1(y, x + 1, Right, right_amount, visited.clone()),
-                solve_part1(y, x - 1, Left, left_amount, visited.clone()),
-            ],
-            Down => [
-                solve_part1(y - 1, x, Down, up_amount, visited.clone()),
-                solve_part1(y, x + 1, Right, right_amount, visited.clone()),
-                solve_part1(y, x - 1, Left, left_amount, visited.clone()),
-            ],
-        }
-        .into_iter()
-        .min()
-        .unwrap_or(10000)
-    } else {
-        10000
+impl Direction {
+    fn not_opposite(self, other: Self) -> bool {
+        !matches!(
+            (self, other),
+            (Left, Right) | (Right, Left) | (Up, Down) | (Down, Up)
+        )
     }
 }
 
 pub fn part1() -> usize {
-    // let (y, x) = MAP.with(|m| (m.len() - 5, m[0].len() - 1));
-    solve_part1(0, 1, Up, 0, VisitedStack(BTreeSet::new()))
+    let (_, score) = dijkstra(
+        &(0, 0, Up, 0),
+        |(y, x, dir, dir_a)| get_neighbors(*y, *x, *dir, *dir_a),
+        |(y, x, _, _)| is_corner(*y, *x),
+    )
+    .unwrap_or_default();
+    score
+}
+
+fn get_ultra_neighbors(
+    y: usize,
+    x: usize,
+    dir: Direction,
+    dir_amount: usize,
+) -> Vec<((usize, usize, Direction, usize), usize)> {
+    let (up_amount, down_amount, left_amount, right_amount) = match dir {
+        Up => (dir_amount + 1, 0, 0, 0),
+        Down => (0, dir_amount + 1, 0, 0),
+        Left => (0, 0, dir_amount + 1, 0),
+        Right => (0, 0, 0, dir_amount + 1),
+    };
+
+    [
+        get(y, x, -1, 0).map(|v| ((y - 1, x, Up, up_amount), v)),
+        get(y, x, 1, 0).map(|v| ((y + 1, x, Down, down_amount), v)),
+        get(y, x, 0, -1).map(|v| ((y, x - 1, Left, left_amount), v)),
+        get(y, x, 0, 1).map(|v| ((y, x + 1, Right, right_amount), v)),
+    ]
+    .into_iter()
+    .flatten()
+    .filter(|((_, _, a, _), _)| a.not_opposite(dir))
+    // .filter(|((_, _, n_dir, d), _)| n_dir.not_opposite(dir) && *d < 3)
+    .filter(|((_, _, n_dir, d), _)| {
+        ((*n_dir != dir && dir_amount >= 3) || *n_dir == dir) && (*d < 10)
+    })
+    .collect()
 }
 
 pub fn part2() -> usize {
-    0
+    dijkstra(
+        &(0, 0, Right, 0),
+        |(y, x, dir, dir_a)| get_ultra_neighbors(*y, *x, *dir, *dir_a),
+        |(y, x, _, d)| is_corner(*y, *x) && *d >= 3,
+    )
+    .unwrap_or_default()
+    .1
+    .min(
+        dijkstra(
+            &(0, 0, Down, 0),
+            |(y, x, dir, dir_a)| get_ultra_neighbors(*y, *x, *dir, *dir_a),
+            |(y, x, _, d)| is_corner(*y, *x) && *d >= 3,
+        )
+        .unwrap_or_default()
+        .1,
+    )
 }
