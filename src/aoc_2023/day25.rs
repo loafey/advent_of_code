55133a6fb837d00765e64bacae8042695b128c06
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-12-26 00:34:27 +0100

    swaggotini

--- src/aoc_2023/day25.rs
+++ src/aoc_2023/day25.rs
@@ -1,9 +1,10 @@
-use std::collections::{BTreeMap as Map, BTreeSet as Set, VecDeque};
-
-use pathfinding::num_traits::PrimInt;
-use rayon::iter::{self, IntoParallelRefIterator, ParallelIterator as _};
+use rand::seq::SliceRandom;
 
 use crate::utils::load_string;
+use std::{
+    collections::{BTreeSet as Set, HashMap as Map},
+    sync::{atomic::AtomicUsize, Arc},
+};
 
 fn input() -> Map<String, Set<String>> {
     let mut map = load_string("inputs/2023/day25.input")
@@ -20,8 +21,9 @@ fn input() -> Map<String, Set<String>> {
             (name, rest)
         })
         .collect::<Map<_, _>>();
+
     let copy = map.clone();
-    // let mut s = "digraph G {".to_string();
+    // let mut s = "digraph G {\n".to_string();
     // for (n, u) in map.iter() {
     //     for u in u {
     //         s.push_str(&format!("   {n} -> {u}\n"));
@@ -41,92 +43,83 @@ fn input() -> Map<String, Set<String>> {
     });
     map
 }
-fn remove_connection(map: &mut Map<String, Set<String>>, start: &String, end: &String) {
-    let start_m = map.get_mut(start).unwrap();
-    start_m.remove(end);
-    let end_m = map.get_mut(end).unwrap();
-    end_m.remove(start);
-}
+// fn remove_connection(map: &mut Map<String, Set<String>>, start: &String, end: &String) {
+//     let start_m = map.get_mut(start).unwrap();
+//     start_m.remove(end);
+//     let end_m = map.get_mut(end).unwrap();
+//     end_m.remove(start);
+// }
 
-fn groupoid(map: &Map<String, Set<String>>) -> Vec<Set<String>> {
-    let mut map = map.clone();
+// fn groupoid(map: &Map<String, Set<String>>) -> Vec<Set<String>> {
+//     let mut map = map.into_iter();
 
-    let mut res = Vec::new();
+//     let mut res = Vec::new();
 
-    while !map.is_empty() {
-        let (n, v) = map.pop_first().unwrap();
-        let mut set = Set::from([n.clone()]);
-        let mut stack = v;
-        while let Some(v) = stack.pop_first() {
-            if !set.contains(&v) {
-                if let Some(conns) = map.remove(&v) {
-                    let conns = conns.clone();
-                    conns.into_iter().for_each(|s| {
-                        stack.insert(s);
-                    });
-                    set.insert(v);
-                }
-            }
-        }
-        res.push(set);
-    }
+//     while !map.is_empty() {
+//         let (n, v) = map.pop_first().unwrap();
+//         let mut set = Set::from([n.clone()]);
+//         let mut stack = v;
+//         while let Some(v) = stack.pop_first() {
+//             if !set.contains(&v) {
+//                 if let Some(conns) = map.remove(&v) {
+//                     let conns = conns.clone();
+//                     conns.into_iter().for_each(|s| {
+//                         stack.insert(s);
+//                     });
+//                     set.insert(v);
+//                 }
+//             }
+//         }
+//         res.push(set);
+//     }
 
-    res
-}
-
-// hfx | pzl
-// bvb | cmg
-// nvd | jqt
-
-// &"hfx".to_string(), &"pzl".to_string(),
-// &"bvb".to_string(), &"cmg".to_string(),
-// &"nvd".to_string(), &"jqt".to_string(),
+//     res
+// }
 pub fn part1() -> usize {
-    let mut map = input();
-    let mut cutters = Set::new();
-    map.iter().for_each(|(n, u)| {
-        for check in u {
-            let check_b = &map[check];
-            if !u.iter().any(|u| check_b.contains(u)) {
-                // Double check if n neighbors is connected to checks neighbors
-                let ab = (n.clone(), check.clone());
-                let ba = (check.clone(), n.clone());
+    let mut sizes = Map::new();
+    (0..10000)
+        .map(|_| {
+            let mut rng = rand::rngs::ThreadRng::default();
+            let mut all = input().into_iter().collect::<Vec<_>>();
+            all.shuffle(&mut rng);
+            let start = all.pop().unwrap();
+            let mut all = all.into_iter().collect::<Map<_, _>>();
+            let mut b = Map::from([start.clone()]);
+            for n in start.1 {
+                let ns = all.remove(&n).unwrap();
+                b.insert(n, ns);
+            }
+            loop {
+                let mut modified = false;
+                let mut new_all = Map::new();
+                let mut vec = all.into_iter().collect::<Vec<_>>();
+                vec.shuffle(&mut rng);
+                for (n, u) in vec {
+                    if u.iter().filter(|a| b.contains_key(*a)).count() > 1 {
+                        modified = true;
+                        b.insert(n, u);
+                    } else {
+                        new_all.insert(n, u);
+                    }
+                }
+                all = new_all;
 
-                if !cutters.contains(&ab) && !cutters.contains(&ba) {
-                    cutters.insert(ab);
+                if !modified {
+                    break;
                 }
             }
-        }
-    });
-    // let mut to_be_removed = Vec::new();
-    // for (a, b) in &cutters {
-    //     for (c, d) in &cutters {
-    //         if (a, b) == (c, d) {
-    //             continue;
-    //         }
-    //         if map[a].contains(c) && map[b].contains(d) {
-    //             to_be_removed.push((a.clone(), b.clone()));
-    //             to_be_removed.push((c.clone(), d.clone()));
-    //         }
-    //         // println!("Pair pair {:?} {:?}", (a, b), (c, d))
-    //     }
-    // }
-    // for t in to_be_removed {
-    //     cutters.remove(&t);
-    // }
-    cutters.iter().for_each(|(a, b)| println!("{a} - {b}"));
-    let (a, b) = cutters.pop_first().unwrap();
-    let (c, d) = cutters.pop_first().unwrap();
-    let (e, f) = cutters.pop_first().unwrap();
-
-    if !cutters.is_empty() {
-        panic!()
-    }
-    remove_connection(&mut map, &a, &b);
-    remove_connection(&mut map, &c, &d);
-    remove_connection(&mut map, &e, &f);
-    let g = groupoid(&map);
-    g[0].len() * g[1].len()
+            all.len() * b.len()
+        })
+        .filter(|a| *a != 0 && *a > 367290)
+        .for_each(|a| {
+            if let Some(val) = sizes.get_mut(&a) {
+                *val += 1;
+            } else {
+                sizes.insert(a, 1);
+            }
+        });
+    println!("{sizes:?}");
+    0
 }
 
 pub fn part2() -> usize {
