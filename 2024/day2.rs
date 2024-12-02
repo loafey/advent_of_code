54378bfebb5d155f7bfaca92e6cf46fd11f1a767
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-02 14:29:05 +0100

    De-spaghetiffy

--- 2024/day2.rs
+++ 2024/day2.rs
@@ -32,35 +32,19 @@ pub fn part1() -> usize {
 }
 pub fn part2() -> i64 {
     let mut safe = 0;
-    'outerouter: for row in include_str!("../inputs/2024/day2.input")
+    'outer: for row in include_str!("../inputs/2024/day2.input")
         .lines()
         .map(|r| r.split_whitespace().map(|s| s.parse::<i64>().unwrap()))
     {
         let row = row.collect::<Vec<_>>();
-        'outer: for row_i in 0..row.len() {
+        for row_i in 0..row.len() {
             let mut row = row.clone();
             row.remove(row_i);
 
-            let mut last: Option<(i64, Ordering)> = None;
-            for num in row {
-                if let Some((lst, dir)) = last {
-                    let diff = lst - num;
-                    if diff.abs() > 3
-                        || lst == num
-                        || matches!(
-                            (dir, lst.cmp(&num)),
-                            (Equal, Equal) | (Less, Greater) | (Greater, Less)
-                        )
-                    {
-                        continue 'outer;
-                    }
-                    last = Some((num, lst.cmp(&num)))
-                } else {
-                    last = Some((num, Ordering::Equal));
-                }
+            if is_alright(row.into_iter()).is_some() {
+                safe += 1;
+                continue 'outer;
             }
-            safe += 1;
-            continue 'outerouter;
         }
     }
     safe
