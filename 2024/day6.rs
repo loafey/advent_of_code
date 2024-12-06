f49f847cf7390fa98fb51c604daeb7c7578036ce
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-06 07:45:30 +0100

    Naiv as darn

--- 2024/day6.rs
+++ 2024/day6.rs
@@ -1,1 +1,128 @@
-pub fn part1() -> i64{0}pub fn part2() -> i64{0}
+use std::collections::{HashMap, HashSet};
+
+use utils::FindSome;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[repr(u8)]
+enum Dir {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+pub fn part1() -> usize {
+    let m = include_str!("../inputs/2024/day6.input")
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let (mut y, mut x) = m
+        .iter()
+        .enumerate()
+        .find_some(|(y, v)| {
+            v.iter().enumerate().find_some(|(x, a)| {
+                if *a == '^' {
+                    Some((y as isize, x as isize))
+                } else {
+                    None
+                }
+            })
+        })
+        .unwrap();
+
+    let mut visited = HashSet::new();
+    let mut dir = Dir::Up;
+    loop {
+        visited.insert((y, x));
+        let (ny, nx) = match dir {
+            Dir::Up => (y - 1, x),
+            Dir::Down => (y + 1, x),
+            Dir::Left => (y, x - 1),
+            Dir::Right => (y, x + 1),
+        };
+        let Some(c) = m.get(ny as usize).and_then(|v| v.get(nx as usize)) else {
+            break;
+        };
+        if *c == '#' {
+            dir = match (dir as u8 + 1) % 4 {
+                0 => Dir::Up,
+                1 => Dir::Right,
+                2 => Dir::Down,
+                3 => Dir::Left,
+                _ => panic!(),
+            };
+        } else {
+            y = ny;
+            x = nx;
+        }
+    }
+
+    visited.len()
+}
+pub fn part2() -> usize {
+    let m = include_str!("../inputs/2024/day6.input")
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let (y, x) = m
+        .iter()
+        .enumerate()
+        .find_some(|(y, v)| {
+            v.iter().enumerate().find_some(|(x, a)| {
+                if *a == '^' {
+                    Some((y as isize, x as isize))
+                } else {
+                    None
+                }
+            })
+        })
+        .unwrap();
+
+    let mut loopy = 0;
+    for py in 0..m.len() {
+        for px in 0..m[py].len() {
+            let mut visited = HashSet::new();
+            let mut dir = Dir::Up;
+            let mut m = m.clone();
+            match m[py][px] {
+                '.' => m[py][px] = '#',
+                _ => continue,
+            }
+            let mut y = y;
+            let mut x = x;
+            loop {
+                // println!("{y}:{x}:{dir:?} {visited:?}");
+                if !visited.insert((y, x, dir)) {
+                    loopy += 1;
+                    break;
+                }
+                let (ny, nx) = match dir {
+                    Dir::Up => (y - 1, x),
+                    Dir::Right => (y, x + 1),
+                    Dir::Down => (y + 1, x),
+                    Dir::Left => (y, x - 1),
+                };
+                let Some(c) = m.get(ny as usize).and_then(|v| v.get(nx as usize)) else {
+                    break;
+                };
+                if *c == '#' {
+                    dir = match (dir as u8 + 1) % 4 {
+                        0 => Dir::Up,
+                        1 => Dir::Right,
+                        2 => Dir::Down,
+                        3 => Dir::Left,
+                        _ => panic!(),
+                    };
+                } else {
+                    y = ny;
+                    x = nx;
+                }
+            }
+        }
+    }
+    loopy
+}
