6ce822ef5a6d25d913a9addd923a80ed46ce7984
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-08 11:01:03 +0100

    Opts!

--- 2024/day8.rs
+++ 2024/day8.rs
@@ -1,14 +1,18 @@
-use rustc_hash::FxHashSet;
+use rustc_hash::{FxHashMap, FxHashSet};
 use utils::bytes_to_matrix;
 
-fn find_chars(mat: &[[u8; 51]; 50], c: u8) -> Vec<(usize, usize)> {
-    let mut ans = Vec::new();
+const Y_MAX: usize = 50;
+const X_MAX: usize = 50;
+type M = &'static [[u8; X_MAX + 1]; Y_MAX];
+
+fn find_chars(mat: M) -> FxHashMap<u8, Vec<(usize, usize)>> {
+    let mut ans: FxHashMap<u8, Vec<(usize, usize)>> = FxHashMap::default();
     #[allow(clippy::needless_range_loop)]
     for y in 0..mat.len() {
         for x in 0..mat[y].len() {
             let nc = mat[y][x];
-            if nc == c {
-                ans.push((y, x));
+            if !matches!(nc, b'.' | b'\n') {
+                ans.entry(nc).or_default().push((y, x));
             }
         }
     }
@@ -16,30 +20,22 @@ fn find_chars(mat: &[[u8; 51]; 50], c: u8) -> Vec<(usize, usize)> {
 }
 
 pub fn part1() -> usize {
-    let m = bytes_to_matrix::<51, 50>(include_bytes!("../inputs/2024/day8.input"));
-
-    let y_max = m.len();
-    let x_max = m[0].len();
+    let m: M = bytes_to_matrix(include_bytes!("../inputs/2024/day8.input"));
+    let chars = find_chars(m);
 
     let mut posses = FxHashSet::default();
-
     #[allow(clippy::needless_range_loop)]
-    for y in 0..y_max {
-        for x in 0..x_max {
-            let c = m[y][x];
-            if !matches!(c, b'#' | b'.') {
-                let others = find_chars(m, c);
-                for (ny, nx) in others {
-                    if (ny, nx) == (y, x) {
-                        continue;
-                    }
+    for (_, others) in chars {
+        for (y, x) in &others {
+            for (ny, nx) in &others {
+                if (ny, nx) == (y, x) {
+                    continue;
+                }
+                let by = y.wrapping_add(y.wrapping_sub(*ny));
+                let bx = x.wrapping_add(x.wrapping_sub(*nx));
 
-                    let by = y.wrapping_add(y.wrapping_sub(ny));
-                    let bx = x.wrapping_add(x.wrapping_sub(nx));
-
-                    if by < y_max && bx < x_max {
-                        posses.insert((by, bx));
-                    }
+                if by < Y_MAX && bx < X_MAX {
+                    posses.insert((by, bx));
                 }
             }
         }
@@ -49,39 +45,31 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    let m = bytes_to_matrix::<51, 50>(include_bytes!("../inputs/2024/day8.input"));
-
-    let y_max = m.len();
-    let x_max = m[0].len();
+    let m: M = bytes_to_matrix(include_bytes!("../inputs/2024/day8.input"));
+    let chars = find_chars(m);
 
     let mut posses = FxHashSet::default();
-
     #[allow(clippy::needless_range_loop)]
-    for y in 0..y_max {
-        for x in 0..x_max {
-            let c = m[y][x];
-            if !matches!(c, b'#' | b'.') {
-                posses.insert((y, x));
-                let others = find_chars(m, c);
-                for (ny, nx) in others {
-                    if (ny, nx) == (y, x) {
-                        continue;
-                    }
+    for (_, others) in chars {
+        for (y, x) in &others {
+            posses.insert((*y, *x));
+            for (ny, nx) in &others {
+                if (ny, nx) == (y, x) {
+                    continue;
+                }
+                let ydiff = y.wrapping_sub(*ny);
+                let xdiff = x.wrapping_sub(*nx);
+                let mut by = y.wrapping_add(ydiff);
+                let mut bx = x.wrapping_add(xdiff);
 
-                    let ydiff = y.wrapping_sub(ny);
-                    let xdiff = x.wrapping_sub(nx);
-                    let mut by = y.wrapping_add(ydiff);
-                    let mut bx = x.wrapping_add(xdiff);
-
-                    loop {
-                        if by < y_max && bx < x_max {
-                            posses.insert((by, bx));
-                        } else {
-                            break;
-                        }
-                        by = by.wrapping_add(ydiff);
-                        bx = bx.wrapping_add(xdiff);
+                loop {
+                    if by < Y_MAX && bx < X_MAX {
+                        posses.insert((by, bx));
+                    } else {
+                        break;
                     }
+                    by = by.wrapping_add(ydiff);
+                    bx = bx.wrapping_add(xdiff);
                 }
             }
         }
