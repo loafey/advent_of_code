6d1e9b2131d48f10636c32614b7d5f49a5c78106
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-12 16:56:20 +0100

    opti opt

--- 2024/day12.rs
+++ 2024/day12.rs
@@ -1,153 +1,151 @@
+use rayon::prelude::*;
 use rustc_hash::FxHashMap;
+use std::sync::Arc;
 use utils::MatrixGet;
 matrixy::matrixy!("../inputs/2024/day12.input");
 
+fn find_shapes(
+    blowup: bool,
+) -> impl ParallelIterator<Item = (FxHashMap<(usize, usize), i32>, usize)> {
+    let map = Arc::new(if !blowup {
+        include_str!("../inputs/2024/day12.input")
+            .lines()
+            .filter(|l| !l.is_empty())
+            .map(|l| l.chars().map(|c| c as u8).collect::<Vec<_>>())
+            .collect::<Vec<_>>()
+    } else {
+        let mut map = include_str!("../inputs/2024/day12.input")
+            .lines()
+            .filter(|l| !l.is_empty())
+            .flat_map(|l| {
+                let s = l
+                    .chars()
+                    .flat_map(|c| [c as u8, c as u8, c as u8])
+                    .collect::<Vec<_>>();
+                [s.clone(), s.clone(), s]
+            })
+            .collect::<Vec<_>>();
+        map.iter_mut().for_each(|r| {
+            r.insert(0, 0);
+            r.push(0);
+        });
+        map.insert(0, vec![0; map[0].len()]);
+        map.push(vec![0; map[0].len()]);
+        map
+    });
+
+    (b'A'..=b'Z')
+        .into_par_iter()
+        .filter_map(move |c| {
+            let mut new_map = vec![vec![0; map[0].len()]; map.len()];
+            let mut found = false;
+            for y in 0..map.len() {
+                for x in 0..map[0].len() {
+                    if map[y][x] == c {
+                        new_map[y][x] = c;
+                        found = true;
+                    }
+                }
+            }
+            if found {
+                Some(new_map)
+            } else {
+                None
+            }
+        })
+        .flat_map(move |mut map| {
+            let mut res = Vec::new();
+            for y in 0..map.len() {
+                for x in 0..map[y].len() {
+                    let c = map[y][x];
+                    if c == 0 {
+                        continue;
+                    }
+                    let mut stack = vec![(y, x)];
+                    let mut to_remove = Vec::new();
+                    while let Some((y, x)) = stack.pop() {
+                        to_remove.push((y, x));
+                        if let Some(v) = map.mget(y, x, -1, 0) {
+                            if *v == c && !to_remove.contains(&(y - 1, x)) {
+                                stack.push((y - 1, x))
+                            }
+                        };
+                        if let Some(v) = map.mget(y, x, 1, 0) {
+                            if *v == c && !to_remove.contains(&(y + 1, x)) {
+                                stack.push((y + 1, x))
+                            }
+                        };
+                        if let Some(v) = map.mget(y, x, 0, -1) {
+                            if *v == c && !to_remove.contains(&(y, x - 1)) {
+                                stack.push((y, x - 1))
+                            }
+                        };
+                        if let Some(v) = map.mget(y, x, 0, 1) {
+                            if *v == c && !to_remove.contains(&(y, x + 1)) {
+                                stack.push((y, x + 1))
+                            }
+                        };
+                    }
+
+                    to_remove.sort();
+                    to_remove.dedup();
+                    let area = to_remove.len();
+                    let mut perimiter = FxHashMap::default();
+                    let mut not_interesting = Vec::new();
+                    for (y, x) in to_remove {
+                        map[y][x] = 0;
+                        perimiter.insert((y, x), 0);
+                        not_interesting.push((y, x));
+                        if blowup {
+                            perimiter.entry((y - 1, x)).or_insert(1);
+                            perimiter.entry((y + 1, x)).or_insert(1);
+                            perimiter.entry((y, x - 1)).or_insert(1);
+                            perimiter.entry((y, x + 1)).or_insert(1);
+                            perimiter.entry((y - 1, x - 1)).or_insert(1);
+                            perimiter.entry((y + 1, x - 1)).or_insert(1);
+                            perimiter.entry((y + 1, x + 1)).or_insert(1);
+                            perimiter.entry((y - 1, x + 1)).or_insert(1);
+                        } else {
+                            *perimiter.entry((y - 1, x)).or_insert(0) += 1;
+                            *perimiter.entry((y + 1, x)).or_insert(0) += 1;
+                            *perimiter.entry((y, x - 1)).or_insert(0) += 1;
+                            *perimiter.entry((y, x + 1)).or_insert(0) += 1;
+                        }
+                    }
+                    for (y, x) in not_interesting {
+                        perimiter.remove(&(y, x));
+                    }
+                    res.push((perimiter, area));
+                }
+            }
+            res
+        })
+}
+
 pub fn part1() -> usize {
-    let mut map = include_str!("../inputs/2024/day12.input")
-        .lines()
-        .filter(|l| !l.is_empty())
-        .map(|l| l.chars().map(|c| c as u8).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    let mut sum = 0;
-    for y in 0..map.len() {
-        for x in 0..map[y].len() {
-            let c = map[y][x];
-            if c == b'.' {
-                continue;
-            }
-            let mut stack = vec![(y, x)];
-            let mut to_remove = Vec::new();
-            while let Some((y, x)) = stack.pop() {
-                to_remove.push((y, x));
-                if let Some(v) = map.mget(y, x, -1, 0) {
-                    if *v == c && !to_remove.contains(&(y - 1, x)) {
-                        stack.push((y - 1, x))
-                    }
-                };
-                if let Some(v) = map.mget(y, x, 1, 0) {
-                    if *v == c && !to_remove.contains(&(y + 1, x)) {
-                        stack.push((y + 1, x))
-                    }
-                };
-                if let Some(v) = map.mget(y, x, 0, -1) {
-                    if *v == c && !to_remove.contains(&(y, x - 1)) {
-                        stack.push((y, x - 1))
-                    }
-                };
-                if let Some(v) = map.mget(y, x, 0, 1) {
-                    if *v == c && !to_remove.contains(&(y, x + 1)) {
-                        stack.push((y, x + 1))
-                    }
-                };
-            }
-
-            to_remove.sort();
-            to_remove.dedup();
-            let area = to_remove.len();
-            let mut perimiter = FxHashMap::default();
-            for (y, x) in to_remove {
-                let y = y + 1;
-                let x = x + 1;
-                perimiter.insert((y, x), 60);
-                *perimiter.entry((y - 1, x)).or_insert(0) += 1;
-                *perimiter.entry((y + 1, x)).or_insert(0) += 1;
-                *perimiter.entry((y, x - 1)).or_insert(0) += 1;
-                *perimiter.entry((y, x + 1)).or_insert(0) += 1;
-                map[y - 1][x - 1] = b'.';
-            }
-            let perimiter = perimiter.into_values().filter(|c| *c <= 4).sum::<usize>();
-            sum += area * perimiter;
-            // println!("{}: {area} | {perimiter}", c as char)
-        }
-    }
-
-    sum
+    find_shapes(false)
+        .map(|(perimiter, area)| {
+            area * perimiter
+                .into_values()
+                .filter(|c| *c <= 4)
+                .map(|s| s as usize)
+                .sum::<usize>()
+        })
+        .sum()
 }
 pub fn part2() -> usize {
-    let mut map = MAP
-        .iter()
-        .flat_map(|s| {
-            let s = s
-                .iter()
-                .filter(|c| **c != b'\n')
-                .copied()
-                .flat_map(|s| [s, s, s])
-                .collect::<Vec<_>>();
-            [s.clone(), s.clone(), s]
-        })
-        .collect::<Vec<_>>();
-    map.iter_mut().for_each(|r| {
-        r.insert(0, 0);
-        r.insert(0, 0);
-        r.insert(0, 0);
-    });
-    map.insert(0, vec![0; map[0].len()]);
-    map.insert(0, vec![0; map[0].len()]);
-    map.insert(0, vec![0; map[0].len()]);
-    map.push(vec![0; map[0].len()]);
+    let perimeters = find_shapes(true);
 
-    let mut sum = 0;
-    for y in 0..map.len() {
-        for x in 0..map[y].len() {
-            let c = map[y][x];
-            if c == 0 {
-                continue;
-            }
-            let mut stack = vec![(y, x)];
-            let mut to_remove = Vec::new();
-            while let Some((y, x)) = stack.pop() {
-                to_remove.push((y, x));
-                if let Some(v) = map.mget(y, x, -1, 0) {
-                    if *v == c && !to_remove.contains(&(y - 1, x)) {
-                        stack.push((y - 1, x))
-                    }
-                };
-                if let Some(v) = map.mget(y, x, 1, 0) {
-                    if *v == c && !to_remove.contains(&(y + 1, x)) {
-                        stack.push((y + 1, x))
-                    }
-                };
-                if let Some(v) = map.mget(y, x, 0, -1) {
-                    if *v == c && !to_remove.contains(&(y, x - 1)) {
-                        stack.push((y, x - 1))
-                    }
-                };
-                if let Some(v) = map.mget(y, x, 0, 1) {
-                    if *v == c && !to_remove.contains(&(y, x + 1)) {
-                        stack.push((y, x + 1))
-                    }
-                };
-            }
-
-            to_remove.sort();
-            to_remove.dedup();
-            let area = to_remove.len();
-            let mut perimiter = FxHashMap::default();
-            let mut not_interesting = Vec::new();
-            for (y, x) in to_remove {
-                map[y][x] = 0;
-                perimiter.insert((y, x), 0);
-                not_interesting.push((y, x));
-                perimiter.entry((y - 1, x)).or_insert(1);
-                perimiter.entry((y + 1, x)).or_insert(1);
-                perimiter.entry((y, x - 1)).or_insert(1);
-                perimiter.entry((y, x + 1)).or_insert(1);
-                perimiter.entry((y - 1, x - 1)).or_insert(1);
-                perimiter.entry((y + 1, x - 1)).or_insert(1);
-                perimiter.entry((y + 1, x + 1)).or_insert(1);
-                perimiter.entry((y - 1, x + 1)).or_insert(1);
-            }
-            for (y, x) in not_interesting {
-                perimiter.remove(&(y, x));
-            }
+    perimeters
+        .into_par_iter()
+        .map(|(mut perimiter, area)| {
             let mut sides = 0;
 
             while !perimiter.is_empty() {
                 let mut keys = perimiter.keys().collect::<Vec<_>>();
                 keys.sort();
                 let mut stack = vec![*keys[0]];
-                let (mut dy, mut dx) = (0, 1);
+                let (mut dy, mut dx) = (0i32, 1i32);
 
                 while let Some((y, x)) = stack.pop() {
                     perimiter.remove(&(y, x));
@@ -179,96 +177,8 @@ pub fn part2() -> usize {
                 }
             }
 
-            // let y = perimiter.keys().map(|(y, _)| *y).max().unwrap() + 5;
-            // let x = perimiter.keys().map(|(_, x)| *x).max().unwrap() + 5;
-            // let mut map = vec![vec![0; x]; y];
-            // for ((y, x), _) in perimiter {
-            //     map[y][x] = 1;
-            // }
-            // for (i, r) in map.iter().enumerate() {
-            //     println!("{i}:\t{r:?}");
-            // }
-            // for y in (0..map.len()).step_by(3) {
-            //     for x in (0..map[y].len()).step_by(3) {
-            //         if y + 2 >= map.len() || x + 2 >= map[y].len() {
-            //             continue;
-            //         }
-            //         if matches!(
-            //             [
-            //                 [map[y][x], map[y][x + 1], map[y][x + 2]],
-            //                 [map[y + 1][x], map[y + 1][x + 1], map[y + 1][x + 2]],
-            //                 [map[y + 2][x], map[y + 2][x + 1], map[y + 2][x + 2]],
-            //             ],
-            //             [[0, _, _], [0, _, _], [1, 0, 0]]
-            //                 | [[1, 0, 0], [0, _, _], [0, _, _]]
-            //                 | [[0, 0, 1], [_, _, 0], [_, _, 0]]
-            //                 | [[_, _, 0], [_, _, 0], [0, 0, 1]]
-            //         ) {
-            //             sides += 2;
-            //         } else if matches!(
-            //             [
-            //                 [map[y][x], map[y][x + 1], map[y][x + 2]],
-            //                 [map[y + 1][x], map[y + 1][x + 1], map[y + 1][x + 2]],
-            //                 [map[y + 2][x], map[y + 2][x + 1], map[y + 2][x + 2]],
-            //             ],
-            //             [[1, 1, 1], [1, _, _], [1, 1, 1]]
-            //                 | [[1, 1, 1], [1, _, 1], [1, _, 1]]
-            //                 | [[1, 1, 1], [_, _, 1], [1, 1, 1]]
-            //                 | [[1, _, 1], [1, _, 1], [1, 1, 1]]
-            //         ) {
-            //             sides += 1;
-            //         } else if matches!(
-            //             [
-            //                 [map[y][x], map[y][x + 1], map[y][x + 2]],
-            //                 [map[y + 1][x], map[y + 1][x + 1], map[y + 1][x + 2]],
-            //                 [map[y + 2][x], map[y + 2][x + 1], map[y + 2][x + 2]],
-            //             ],
-            //             [[1, _, _], [1, _, _], [1, 1, 1]]
-            //                 | [[1, 1, 1], [1, _, _], [1, _, _]]
-            //                 | [[1, 1, 1], [_, _, 1], [_, _, 1]]
-            //                 | [[_, _, 1], [_, _, 1], [1, 1, 1]]
-            //         ) {
-            //             sides += 1;
-            //         }
-            //     }
-            // }
-            // while !perimiter.is_empty() {
-            //     let (cor, _) = perimiter.iter().next().unwrap();
-            //     let cor = *cor;
-            //     sides += 1;
-            //     let mut stack = vec![cor];
-            //     let mut up_down = None;
-            //     // println!("\n{}", c as char);
-            //     while let Some((y, x)) = stack.pop() {
-            //         // println!("{up_down:?}");
-            //         perimiter.remove(&(y, x));
-            //         if perimiter.contains_key(&(y - 1, x)) && up_down.unwrap_or(true) {
-            //             up_down = Some(true);
-            //             stack.push((y - 1, x));
-            //         } else if perimiter.contains_key(&(y, x - 1)) && !up_down.unwrap_or_default() {
-            //             up_down = Some(false);
-            //             stack.push((y, x - 1));
-            //         }
-            //         if perimiter.contains_key(&(y + 1, x)) && up_down.unwrap_or(true) {
-            //             up_down = Some(true);
-            //             stack.push((y + 1, x));
-            //         } else if perimiter.contains_key(&(y, x + 1)) && !up_down.unwrap_or_default() {
-            //             up_down = Some(false);
-            //             stack.push((y, x + 1));
-            //         }
-            //     }
-            //     // println!()
-            //     // println!("{v}");
-            // }
             let area = area / 9;
-            // println!("{}: {area} * {sides}", c as char);
-            sum += area * sides;
-            // println!("{}: {area} | {perimiter}", c as char)
-        }
-    }
-
-    sum
+            area * sides
+        })
+        .sum()
 }
-
-// 876156 >
-// 946084 <
