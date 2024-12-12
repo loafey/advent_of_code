9926adbf52de035da64f1e7dd56b6f568e95e041
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-12 10:07:58 +0100

    start work

--- 2024/day12.rs
+++ 2024/day12.rs
@@ -68,6 +68,92 @@ pub fn part1() -> usize {
 
     sum
 }
-pub fn part2() -> i64 {
-    0
+pub fn part2() -> usize {
+    let mut map = MAP
+        .iter()
+        .map(|s| {
+            s.iter()
+                .filter(|c| **c != b'\n')
+                .copied()
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    map.insert(0, vec![0; map[0].len()]);
+    map.iter_mut().for_each(|r| {
+        r.push(0);
+        r.insert(0, 0);
+    });
+    map.push(map[0].clone());
+    for r in &map {
+        for c in r {
+            print!("{c}\t")
+        }
+        println!()
+    }
+
+    let mut sum = 0;
+    for y in 0..map.len() {
+        for x in 0..map[y].len() {
+            let c = map[y][x];
+            if c <= 4 {
+                continue;
+            }
+            let mut stack = vec![(y, x)];
+            let mut to_remove = Vec::new();
+            while let Some((y, x)) = stack.pop() {
+                to_remove.push((y, x));
+                if let Some(v) = map.mget(y, x, -1, 0) {
+                    if *v == c && !to_remove.contains(&(y - 1, x)) {
+                        stack.push((y - 1, x))
+                    }
+                };
+                if let Some(v) = map.mget(y, x, 1, 0) {
+                    if *v == c && !to_remove.contains(&(y + 1, x)) {
+                        stack.push((y + 1, x))
+                    }
+                };
+                if let Some(v) = map.mget(y, x, 0, -1) {
+                    if *v == c && !to_remove.contains(&(y, x - 1)) {
+                        stack.push((y, x - 1))
+                    }
+                };
+                if let Some(v) = map.mget(y, x, 0, 1) {
+                    if *v == c && !to_remove.contains(&(y, x + 1)) {
+                        stack.push((y, x + 1))
+                    }
+                };
+            }
+
+            to_remove.sort();
+            to_remove.dedup();
+            let area = to_remove.len();
+            let mut perimiter = FxHashMap::default();
+            for (y, x) in to_remove {
+                perimiter.insert((y, x), 60);
+                *perimiter.entry((y - 1, x)).or_insert(0) += 1;
+                *perimiter.entry((y + 1, x)).or_insert(0) += 1;
+                *perimiter.entry((y, x - 1)).or_insert(0) += 1;
+                *perimiter.entry((y, x + 1)).or_insert(0) += 1;
+                map[y][x] = b'.';
+            }
+            for ((y, x), v) in &perimiter {
+                if *v <= 4 {
+                    map[*y][*x] += *v as u8;
+                }
+            }
+            println!();
+            let perimiter = perimiter.into_values().filter(|c| *c <= 4).sum::<usize>();
+            sum += area * perimiter;
+            // println!("{}: {area} | {perimiter}", c as char)
+
+            for r in &map {
+                for c in r {
+                    print!("{c}\t")
+                }
+                println!()
+            }
+        }
+    }
+
+    sum
 }
