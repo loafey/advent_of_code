bd3a14cdf39721f09a2bf8f4e05103d634d9af16
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-13 11:35:34 +0100

    math

--- 2024/day13.rs
+++ 2024/day13.rs
@@ -9,71 +9,13 @@ struct Game {
 }
 
 fn solve((ax, ay): (i64, i64), (bx, by): (i64, i64), (gx, gy): (i64, i64)) -> Option<(i64, i64)> {
-    // println!("{a:?} {b:?} {g:?}");
-    let mut b_div = gx / bx;
-    println!("starting a at: {b_div}");
-    let mut binary = 1;
-    let binary_mod = 2;
-    loop {
-        // println!("{b_div} {binary}");
-        // std::thread::sleep(std::time::Duration::from_millis(200));
-        let x_diff = gx / (bx * b_div);
-        let y_diff = gy / (by * b_div);
-        if x_diff >= 1 && y_diff >= 1 {
-            if binary != 1 {
-                b_div += binary / binary_mod;
-                b_div -= 1;
-                binary = 1;
-            } else {
-                break;
-            }
-        } else {
-            b_div -= binary;
-            binary *= binary_mod;
-        }
-
-        if b_div <= 0 {
-            if binary != 1 {
-                b_div += binary / binary_mod;
-                binary = 1;
-            } else {
-                return None;
-            }
-        }
+    let m = (ax * gy - gx * ay) / (ax * by - bx * ay);
+    let n = (gx - m * bx) / ax;
+    if n * ax + m * bx == gx && n * ay + m * by == gy {
+        Some((m, n))
+    } else {
+        None
     }
-
-    let mut binary = 1;
-    let mut a_div = gx / ax;
-    println!("starting b at: {b_div}");
-    loop {
-        let x_diff = gx as f64 / ((bx * b_div) + (ax * a_div)) as f64;
-        let y_diff = gy as f64 / ((by * b_div) + (ay * a_div)) as f64;
-
-        println!("{binary}: {a_div} {b_div} {x_diff} {y_diff}");
-        if x_diff != 1.0 || y_diff != 1.0 {
-            println!("bin!");
-            a_div -= binary;
-            binary *= binary_mod;
-            if a_div == 0 {
-                b_div -= 1;
-                a_div = gx / ax;
-                binary = 1;
-            }
-        }
-        if x_diff == 1.0 && y_diff == 1.0 {
-            break;
-        }
-
-        if b_div <= 0 {
-            if binary != 1 {
-                a_div += binary / binary_mod;
-                binary = 1;
-            } else {
-                return None;
-            }
-        }
-    }
-    Some((b_div, a_div))
 }
 
 fn parse_and_solve(xmod: i64, ymod: i64) -> i64 {
