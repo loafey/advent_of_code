6ae1c0be2f2b3e3cb7f39c7ea96988c7bb868fe9
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-13 11:37:20 +0100

    Done

--- 2024/day13.rs
+++ 2024/day13.rs
@@ -1,12 +1,4 @@
-// use rayon::prelude::*;
-use utils::NumExt;
-
-#[derive(Debug)]
-struct Game {
-    a: (i64, i64),
-    b: (i64, i64),
-    goal: (i64, i64),
-}
+use rayon::prelude::*;
 
 fn solve((ax, ay): (i64, i64), (bx, by): (i64, i64), (gx, gy): (i64, i64)) -> Option<(i64, i64)> {
     let m = (ax * gy - gx * ay) / (ax * by - bx * ay);
@@ -23,48 +15,30 @@ fn parse_and_solve(xmod: i64, ymod: i64) -> i64 {
         .split("\n\n")
         .filter(|s| !s.is_empty())
         .map(|r| {
+            let s = [' ', 'X', '+', ',', 'Y', ':', 'A', 'B', '='];
             let mut l = r.lines();
             let a = l.next().unwrap();
-            let mut a = a
-                .split([' ', 'X', '+', ',', 'Y', ':', 'A', 'B'])
-                .filter(|s| !s.is_empty())
-                .skip(1);
+            let mut a = a.split(s).filter(|s| !s.is_empty()).skip(1);
             let a1 = a.next().unwrap().parse::<i64>().unwrap();
             let a2 = a.next().unwrap().parse::<i64>().unwrap();
 
             let b = l.next().unwrap();
-            let mut b = b
-                .split([' ', 'X', '+', ',', 'Y', ':', 'A', 'B'])
-                .filter(|s| !s.is_empty())
-                .skip(1);
+            let mut b = b.split(s).filter(|s| !s.is_empty()).skip(1);
             let b1 = b.next().unwrap().parse::<i64>().unwrap();
             let b2 = b.next().unwrap().parse::<i64>().unwrap();
 
             let p = l.next().unwrap();
-            let mut p = p
-                .split([' ', 'X', '+', ',', 'Y', ':', 'A', 'B', '='])
-                .filter(|s| !s.is_empty())
-                .skip(1);
+            let mut p = p.split(s).filter(|s| !s.is_empty()).skip(1);
             let p1 = p.next().unwrap().parse::<i64>().unwrap();
             let p2 = p.next().unwrap().parse::<i64>().unwrap();
 
-            Game {
-                a: (a1, a2),
-                b: (b1, b2),
-                goal: (p1, p2),
-            }
+            ((a1, a2), (b1, b2), (p1, p2))
         })
         .collect::<Vec<_>>();
 
     input
         .into_iter()
-        .filter_map(
-            |Game {
-                 a,
-                 b,
-                 goal: (gx, gy),
-             }| solve(a, b, (gx + xmod, gy + ymod)),
-        )
+        .filter_map(|(a, b, (gx, gy))| solve(a, b, (gx + xmod, gy + ymod)))
         .map(|(b, a)| a * 3 + b)
         .sum()
 }
