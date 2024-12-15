265e1eed8ef4acb4359b99952e629e1388181d61
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-15 12:14:55 +0100

    small cleanup

--- 2024/day15.rs
+++ 2024/day15.rs
@@ -1,20 +1,6 @@
 use utils::{FindSome, MatrixGet};
 
-pub fn part1() -> usize {
-    let (map, moves) = include_str!("../inputs/2024/day15.input")
-        .split_once("\n\n")
-        .unwrap();
-    let mut map = map
-        .lines()
-        .filter(|s| !s.is_empty())
-        .map(|s| s.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    let moves = moves
-        .lines()
-        .filter(|s| !s.is_empty())
-        .flat_map(|c| c.chars())
-        .collect::<Vec<_>>();
-
+fn solve(mut map: Vec<Vec<char>>, moves: Vec<char>) -> usize {
     let (mut by, mut bx) = map
         .iter()
         .enumerate()
@@ -26,107 +12,6 @@ pub fn part1() -> usize {
         .unwrap();
 
     for m in moves {
-        let (dy, dx) = match m {
-            '^' => (-1, 0),
-            '<' => (0, -1),
-            '>' => (0, 1),
-            'v' => (1, 0),
-            _ => panic!(),
-        };
-
-        let other = map.mget(by, bx, dy, dx).unwrap();
-        match other {
-            '.' => {
-                map[by][bx] = '.';
-                by = (by as isize + dy) as usize;
-                bx = (bx as isize + dx) as usize;
-                map[by][bx] = '@';
-            }
-            'O' => {
-                let (mut ty, mut tx) = ((by as isize + dy) as usize, (bx as isize + dx) as usize);
-                let (sy, sx) = (ty, tx);
-                let mut to_move = vec![(ty, tx)];
-                loop {
-                    let other = map.mget(ty, tx, 0, 0).unwrap();
-                    match other {
-                        '.' => {
-                            while let Some((ty, tx)) = to_move.pop() {
-                                map[ty][tx] = '.';
-                                let (ty, tx) =
-                                    ((ty as isize + dy) as usize, (tx as isize + dx) as usize);
-                                map[ty][tx] = 'O';
-                            }
-                            map[by][bx] = '.';
-                            map[sy][sx] = '@';
-                            (by, bx) = (sy, sx);
-                            break;
-                        }
-                        '#' => {
-                            break;
-                        }
-                        'O' => {
-                            to_move.push((ty, tx));
-                            (ty, tx) = ((ty as isize + dy) as usize, (tx as isize + dx) as usize);
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            _ => {}
-        }
-    }
-
-    map.iter()
-        .enumerate()
-        .map(|(y, r)| {
-            r.iter()
-                .enumerate()
-                .map(|(x, c)| if *c == 'O' { 100 * y + x } else { 0 })
-                .sum::<usize>()
-        })
-        .sum()
-}
-pub fn part2() -> usize {
-    let (map, moves) = include_str!("../inputs/2024/day15.input")
-        .split_once("\n\n")
-        .unwrap();
-    let mut map = map
-        .lines()
-        .filter(|s| !s.is_empty())
-        .map(|s| {
-            s.chars()
-                .flat_map(|c| match c {
-                    '#' => ['#', '#'],
-                    'O' => ['[', ']'],
-                    '.' => ['.', '.'],
-                    '@' => ['@', '.'],
-                    _ => panic!(),
-                })
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-    let moves = moves
-        .lines()
-        .filter(|s| !s.is_empty())
-        .flat_map(|c| c.chars())
-        .collect::<Vec<_>>();
-
-    let (mut by, mut bx) = map
-        .iter()
-        .enumerate()
-        .find_some(|(y, r)| {
-            r.iter()
-                .enumerate()
-                .find_some(|(x, c)| if *c == '@' { Some((y, x)) } else { None })
-        })
-        .unwrap();
-
-    map.iter().for_each(|v| {
-        v.iter().for_each(|c| print!("{c}"));
-        println!()
-    });
-    for m in moves {
-        println!("\nMove: {m}");
         let (dy, dx) = match m {
             '^' => (-1, 0),
             '<' => (0, -1),
@@ -252,14 +137,6 @@ pub fn part2() -> usize {
             }
             _ => {}
         }
-
-        map.iter().for_each(|v| {
-            v.iter().for_each(|c| print!("{c}"));
-            println!()
-        });
-
-        // let mut buffer = String::new();
-        // std::io::stdin().read_line(&mut buffer).unwrap();
     }
 
     map.iter()
@@ -267,11 +144,61 @@ pub fn part2() -> usize {
         .map(|(y, r)| {
             r.iter()
                 .enumerate()
-                .map(|(x, c)| if *c == '[' { 100 * y + x } else { 0 })
+                .map(|(x, c)| {
+                    if matches!(*c, '[' | 'O') {
+                        100 * y + x
+                    } else {
+                        0
+                    }
+                })
                 .filter(|s| *s > 0)
                 .sum::<usize>()
         })
         .sum()
+}
+
+pub fn part1() -> usize {
+    let (map, moves) = include_str!("../inputs/2024/day15.input")
+        .split_once("\n\n")
+        .unwrap();
+    let map = map
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let moves = moves
+        .lines()
+        .filter(|s| !s.is_empty())
+        .flat_map(|c| c.chars())
+        .collect::<Vec<_>>();
+
+    solve(map, moves)
+}
+pub fn part2() -> usize {
+    let (map, moves) = include_str!("../inputs/2024/day15.input")
+        .split_once("\n\n")
+        .unwrap();
+    let map = map
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|s| {
+            s.chars()
+                .flat_map(|c| match c {
+                    '#' => ['#', '#'],
+                    'O' => ['[', ']'],
+                    '.' => ['.', '.'],
+                    '@' => ['@', '.'],
+                    _ => panic!(),
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let moves = moves
+        .lines()
+        .filter(|s| !s.is_empty())
+        .flat_map(|c| c.chars())
+        .collect::<Vec<_>>();
+    solve(map, moves)
 }
 
 // > 1495651
