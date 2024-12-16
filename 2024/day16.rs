dfd50c4a3cb825ed5271af94d3d91f1a6f4c79e8
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-16 21:51:45 +0100

    Add comment

--- 2024/day16.rs
+++ 2024/day16.rs
@@ -1,6 +1,11 @@
-use pathfinding::prelude::{dijkstra, yen};
+use pathfinding::prelude::{astar_bag, dijkstra};
 use rustc_hash::FxHashSet;
-use utils::{Direction, Direction::*, FindSome};
+use utils::{
+    Direction::{self, *},
+    FindSome, NumTupleExt,
+};
+
+// This day was completely solved by my fantastic pathfinding lib :)
 
 fn find(g: u8, map: Map) -> (usize, usize) {
     map.iter()
@@ -57,152 +62,19 @@ pub fn part1() -> usize {
     .1
 }
 
-// #[allow(clippy::type_complexity)]
-// fn expr() -> FxHashMap<(usize, usize, Direction), Vec<((usize, usize, Direction), i32)>> {
-//     let (sy, sx) = find(b'S', MAP);
-//     let (ey, ex) = find(b'E', MAP);
-//     let mut map = FxHashMap::default();
-//     let mut visited = FxHashSet::default();
-//     let mut stack = vec![(sy, sx, East)];
-//     while let Some((y, x, d)) = stack.pop() {
-//         if !visited.insert((y, x, d)) {
-//             continue;
-//         }
-
-//         let mut r = Vec::new();
-//         let (dy, dx) = match d {
-//             North => (y - 1, x),
-//             East => (y, x + 1),
-//             South => (y + 1, x),
-//             West => (y, x - 1),
-//         };
-//         if MAP[dy][dx] != b'#' {
-//             r.push(((dy, dx, d), 1));
-//             stack.push((dy, dx, d));
-//         }
-//         let (ky, kx) = match d.rotate_r() {
-//             North => (y - 1, x),
-//             East => (y, x + 1),
-//             South => (y + 1, x),
-//             West => (y, x - 1),
-//         };
-//         if MAP[ky][kx] != b'#' {
-//             r.push(((y, x, d.rotate_r()), 1000));
-//             stack.push((y, x, d.rotate_r()));
-//         }
-//         let (ky, kx) = match d.rotate_l() {
-//             North => (y - 1, x),
-//             East => (y, x + 1),
-//             South => (y + 1, x),
-//             West => (y, x - 1),
-//         };
-//         if MAP[ky][kx] != b'#' {
-//             r.push(((y, x, d.rotate_l()), 1000));
-//             stack.push((y, x, d.rotate_l()));
-//         }
-
-//         map.insert((y, x, d), r);
-//     }
-
-//     let mut last = 0;
-//     loop {
-//         let to_remove = map
-//             .iter()
-//             .filter(|(m, v)| v.len() == 1 && !(m.0 == ey && m.1 == ex) && !(m.0 == sy && m.1 == sx))
-//             .map(|(k, v)| (*k, v[0]))
-//             .collect::<Vec<_>>();
-//         for (k, (d1, w1)) in &to_remove {
-//             for n in map.values_mut() {
-//                 for (d2, w2) in n {
-//                     // println!("{k:?} == {d2:?}");
-//                     if *d2 == *k {
-//                         *d2 = *d1;
-//                         *w2 += w1;
-//                         break;
-//                     }
-//                 }
-//             }
-//         }
-//         for (k, _) in to_remove {
-//             map.remove(&k);
-//         }
-
-//         let to_remove = map
-//             .iter()
-//             .filter(|(_, v)| v.is_empty())
-//             .map(|(k, _)| *k)
-//             .collect::<Vec<_>>();
-//         for k in &to_remove {
-//             for n in map.values_mut() {
-//                 if let Some((i, _)) = n.iter().enumerate().find(|(_, (k2, _))| *k == *k2) {
-//                     n.remove(i);
-//                 }
-//             }
-//         }
-//         for k in to_remove {
-//             map.remove(&k);
-//         }
-
-//         if last == map.len() {
-//             break;
-//         }
-//         last = map.len();
-//     }
-
-//     for (v, d) in &map {
-//         println!("{v:?}:\t{d:?}")
-//     }
-
-//     let mut draw = MAP
-//         .iter()
-//         .map(|r| {
-//             r.iter()
-//                 .filter(|c| **c != b'\n')
-//                 .map(|c| *c as char)
-//                 .map(|c| if c == '.' { ' ' } else { c })
-//                 .collect::<Vec<_>>()
-//         })
-//         .collect::<Vec<_>>();
-//     for k in map.keys() {
-//         let (y, x, d) = k;
-//         if draw[*y][*x] == ' ' {
-//             draw[*y][*x] = match d {
-//                 North => '^',
-//                 East => '>',
-//                 South => 'v',
-//                 West => '<',
-//             };
-//         }
-//     }
-//     for r in draw {
-//         for c in r {
-//             print!("{c}")
-//         }
-//         println!()
-//     }
-
-//     map
-// }
-
 pub fn part2() -> usize {
-    // expr();
-
     let (sy, sx) = find(b'S', MAP);
     let (ey, ex) = find(b'E', MAP);
 
-    let mut paths = yen(
+    astar_bag(
         &(sy, sx, East),
         successors,
+        |(y, x, _)| (*y, *x).manhattan_distance(&(ey, ex)),
         |(y, x, _)| (*y, *x) == (ey, ex),
-        18,
-    );
-    paths.sort_by_key(|(_, s)| *s);
-    let k = paths[0].1;
-    paths.retain(|(_, s)| *s == k);
-
-    paths
-        .into_iter()
-        .flat_map(|(v, _)| v.into_iter().map(|(y, x, _)| (y, x)))
-        .collect::<FxHashSet<_>>()
-        .len()
+    )
+    .unwrap()
+    .0
+    .flat_map(|v| v.into_iter().map(|(y, x, _)| (y, x)))
+    .collect::<FxHashSet<_>>()
+    .len()
 }
