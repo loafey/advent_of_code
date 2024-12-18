f7847e1e2c8f389f699a33fe43869662164f9758
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-18 14:06:44 +0100

    Cut down

--- 2024/day18.rs
+++ 2024/day18.rs
@@ -1,6 +1,25 @@
 use pathfinding::prelude::dijkstra;
+use std::collections::BTreeSet;
 use utils::{bi_functors::BiFunctorExtExt, MatrixGet};
 
+fn successors(grid: &[[char; 71]; 71], y: usize, x: usize) -> Vec<((usize, usize), usize)> {
+    let mut r = Vec::new();
+    if let Some('.') = grid.mget(y, x, -1, 0) {
+        r.push(((y - 1, x), 1));
+    }
+    if let Some('.') = grid.mget(y, x, 1, 0) {
+        r.push(((y + 1, x), 1));
+    }
+    if let Some('.') = grid.mget(y, x, 0, -1) {
+        r.push(((y, x - 1), 1));
+    }
+    if let Some('.') = grid.mget(y, x, 0, 1) {
+        r.push(((y, x + 1), 1));
+    }
+    r
+}
+
+const END: (usize, usize) = (70, 70);
 pub fn part1() -> usize {
     let coords = include_str!("../inputs/2024/day18.input")
         .lines()
@@ -9,43 +28,20 @@ pub fn part1() -> usize {
             s.split_once(',')
                 .unwrap()
                 .splet(|s| s.parse::<usize>().unwrap())
-        })
-        .collect::<Vec<_>>();
+        });
 
     let mut grid = [['.'; 71]; 71];
-    let start = (0, 0);
-    let end = (70, 70);
 
-    for (i, (x, y)) in coords.iter().cycle().enumerate() {
+    for (i, (x, y)) in coords.enumerate() {
         if i >= 1024 {
             break;
         }
-        grid[*y][*x] = '#';
+        grid[y][x] = '#';
     }
 
-    let (path, len) = dijkstra(
-        &start,
-        |(y, x)| {
-            let mut r = Vec::new();
-            if let Some('.') = grid.mget(*y, *x, -1, 0) {
-                r.push(((y - 1, *x), 1));
-            }
-            if let Some('.') = grid.mget(*y, *x, 1, 0) {
-                r.push(((y + 1, *x), 1));
-            }
-            if let Some('.') = grid.mget(*y, *x, 0, -1) {
-                r.push(((*y, x - 1), 1));
-            }
-            if let Some('.') = grid.mget(*y, *x, 0, 1) {
-                r.push(((*y, x + 1), 1));
-            }
-            r
-        },
-        |c| *c == end,
-    )
-    .unwrap();
-
-    len
+    dijkstra(&(0, 0), |(y, x)| successors(&grid, *y, *x), |c| *c == END)
+        .unwrap()
+        .1
 }
 pub fn part2() -> String {
     let coords = include_str!("../inputs/2024/day18.input")
@@ -55,40 +51,24 @@ pub fn part2() -> String {
             s.split_once(',')
                 .unwrap()
                 .splet(|s| s.parse::<usize>().unwrap())
-        })
-        .collect::<Vec<_>>();
+        });
 
     let mut grid = [['.'; 71]; 71];
-    let start = (0, 0);
-    let end = (70, 70);
+    let mut path = BTreeSet::default();
 
-    for (i, (x, y)) in coords.iter().cycle().enumerate() {
-        grid[*y][*x] = '#';
+    for (i, (x, y)) in coords.enumerate() {
+        grid[y][x] = '#';
+        if i < 2000 || (!path.is_empty() && !path.contains(&(y, x))) {
+            continue;
+        }
 
-        if dijkstra(
-            &start,
-            |(y, x)| {
-                let mut r = Vec::new();
-                if let Some('.') = grid.mget(*y, *x, -1, 0) {
-                    r.push(((y - 1, *x), 1));
-                }
-                if let Some('.') = grid.mget(*y, *x, 1, 0) {
-                    r.push(((y + 1, *x), 1));
-                }
-                if let Some('.') = grid.mget(*y, *x, 0, -1) {
-                    r.push(((*y, x - 1), 1));
-                }
-                if let Some('.') = grid.mget(*y, *x, 0, 1) {
-                    r.push(((*y, x + 1), 1));
-                }
-                r
-            },
-            |c| *c == end,
-        )
-        .is_none()
+        if let Some(sp) = dijkstra(&(0, 0), |(y, x)| successors(&grid, *y, *x), |c| *c == END)
+            .map(|(s, _)| BTreeSet::from_iter(s.into_iter()))
         {
+            path = sp;
+        } else {
             return format!("{x},{y}");
         }
     }
-    "oops".to_string()
+    String::new()
 }
