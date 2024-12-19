84c123a7e728e35021bd079288efdb59de737ed5
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-19 14:22:27 +0100

    De opt

--- 2024/day19.rs
+++ 2024/day19.rs
@@ -1,13 +1,8 @@
-#![allow(static_mut_refs, mutable_transmutes)]
+#![allow(static_mut_refs)]
 // why memoize this when you can have mutable global variables :)
 static mut COLORS: Vec<&'static str> = Vec::new();
-#[thread_local]
-static CACHE: LazyCell<Cache> = LazyCell::new(Cache::default);
 
 use rayon::prelude::*;
-use rustc_hash::FxHashMap;
-type Cache = FxHashMap<usize, usize>;
-use std::{cell::LazyCell, mem::transmute};
 
 fn solve(f: fn(&'static str) -> usize) -> usize {
     let (colors, designs) = include_str!("../inputs/2024/day19.input")
@@ -20,25 +15,19 @@ fn solve(f: fn(&'static str) -> usize) -> usize {
         .lines()
         .filter(|s| !s.is_empty())
         .par_bridge()
-        .map(|d| {
-            unsafe { transmute::<&Cache, &mut Cache>(&*CACHE) }.clear();
-            f(d)
-        })
+        .map(f)
         .sum()
 }
 
+#[memoize::memoize]
 fn rec(d: &'static str) -> usize {
-    if let Some(v) = CACHE.get(&d.len()) {
-        return *v;
-    } else if d.is_empty() {
+    if d.is_empty() {
         return 1;
     }
-    let u = unsafe { &COLORS }
+    unsafe { &COLORS }
         .iter()
         .map(|c| d.strip_prefix(c).map(rec).unwrap_or_default())
-        .sum::<usize>();
-    unsafe { transmute::<&Cache, &mut Cache>(&*CACHE) }.insert(d.len(), u);
-    u
+        .sum()
 }
 
 pub fn part1() -> usize {
