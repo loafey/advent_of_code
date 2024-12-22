88f66d03166f61e8ae81b484ca36256670ad14f8
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-12-22 12:40:46 +0100

    It somewhat slow, but it fine

--- 2024/day22.rs
+++ 2024/day22.rs
@@ -47,22 +47,21 @@ pub fn part2() -> i64 {
         .map(|s| s.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
     let mut lookup: FxHashMap<i32, FxHashMap<i64, i64>> = FxHashMap::default();
-    for code in &codes {
+    codes.iter().for_each(|code| {
         let og_code = *code;
         let mut code = *code;
-        let mut seq = VecDeque::new();
+        let mut s = VecDeque::new();
         for _ in 0..2000 {
             let (nc, price, change) = math(code);
-            seq.push_back(change);
-            if seq.len() > 4 {
-                seq.pop_front();
-                let r: [i8; 4] = [seq[0], seq[1], seq[2], seq[3]];
-                let r = unsafe { std::mem::transmute::<[i8; 4], i32>(r) };
+            s.push_back(change);
+            if s.len() > 4 {
+                s.pop_front();
+                let r = unsafe { std::mem::transmute::<[i8; 4], i32>([s[0], s[1], s[2], s[3]]) };
                 lookup.entry(r).or_default().entry(og_code).or_insert(price);
             }
             code = nc;
         }
-    }
+    });
     lookup
         .into_values()
         .map(|v| v.into_values().sum())
