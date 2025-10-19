00898ceb6520a0eeb92b48a80bda860941848366
Samuel Hammersberg <samuel@loafey.se>
2025-10-19 10:12:15 +0200

    Update styling.

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,4 +1,4 @@
-mkdir benchmarks-data
+mkdir benchmarks-data 2> /dev/null
 
 YEARS=(
     # 2025
@@ -15,44 +15,79 @@ YEARS=(
 )
 site="<!DOCTYPE html><html><head><meta charset=\"UTF-8\" /><title>Advent of Code</title></head><body>"
 
+emoji () {
+    if [[ $1 == *"ns"* ]]; then
+        echo "💅"
+    elif [[ $1 == *"µs"* ]]; then
+        echo "👑"
+    elif [[ $1 == *"ms"* ]]; then
+        echo "🥈"
+    else 
+        echo "🥉"
+    fi
+}
+
+textSettings=""
 for year in "${YEARS[@]}"; do
     output="<svg height=\"830\" width=\"1300\" xmlns=\"http://www.w3.org/2000/svg\">"
-    output="$output<rect width=\"100%\" height=\"100%\" fill=\"%230D1117\" />"
-    output="$output<text x=\"10\" y=\"30\" fill=\"white\">Year $year</text>"
-    output="$output<text x=\"305\" y=\"30\" fill=\"white\">Part 1</text>"
-    output="$output<text x=\"905\" y=\"30\" fill=\"white\">Part 2</text>"
+    output="$output<style>"
+    output="$output text { font-family: \"Arial\", monospace; }"
+    output="$output rect { fill: transparent }"
+    output="$output text { fill: black }"
+    output="$output @media (prefers-color-scheme: dark) {"
+    output="$output text { fill: white }"
+    output="$output rect { fill: #0D1117 }"
+    output="$output } </style>"
+    output="$output<rect width=\"100%\" height=\"100%\" />"
+    output="$output<text x=\"10\" y=\"30\" $textSettings>Year $year</text>"
+    output="$output<text x=\"305\" y=\"30\" $textSettings>Part 1</text>"
+    output="$output<text x=\"905\" y=\"30\" $textSettings>Part 2</text>"
 
-    output="$output<text x=\"105\" y=\"60\" fill=\"white\">Average</text>"
-    output="$output<text x=\"305\" y=\"60\" fill=\"white\">Best</text>"
-    output="$output<text x=\"505\" y=\"60\" fill=\"white\">Worst</text>"
+    output="$output<text x=\"105\" y=\"60\" $textSettings>Average</text>"
+    output="$output<text x=\"305\" y=\"60\" $textSettings>Best</text>"
+    output="$output<text x=\"505\" y=\"60\" $textSettings>Worst</text>"
 
-    output="$output<text x=\"705\" y=\"60\" fill=\"white\">Average</text>"
-    output="$output<text x=\"905\" y=\"60\" fill=\"white\">Best</text>"
-    output="$output<text x=\"1105\" y=\"60\" fill=\"white\">Worst</text>"
+    output="$output<text x=\"705\" y=\"60\" $textSettings>Average</text>"
+    output="$output<text x=\"905\" y=\"60\" $textSettings>Best</text>"
+    output="$output<text x=\"1105\" y=\"60\" $textSettings>Worst</text>"
     for day in $(seq 1 25); do
         echo "$year - $day"
         y=$(echo $(($day*30+60))) 
-        output="$output<text x=\"5\" y=\"$y\" fill=\"white\">Day $day:</text>"
+        output="$output<text x=\"5\" y=\"$y\" $textSettings>Day $day:</text>"
         part1=$(ulimit -v 4000000 -t 100 && ./target/release/advent_of_code -s $year $day 1)
         part2=$(ulimit -v 4000000 -t 100 && ./target/release/advent_of_code -s $year $day 2)
         if [ -n "$part1" ]; then
-            output="$output<text x=\"105\" y=\"$y\" fill=\"white\">$(echo "$part1" | jq .avg -r)</text>"
-            output="$output<text x=\"305\" y=\"$y\" fill=\"white\">$(echo "$part1" | jq .best -r)</text>"
-            output="$output<text x=\"505\" y=\"$y\" fill=\"white\">$(echo "$part1" | jq .worst -r)</text>"
+            avg=$(echo "$part1" | jq .avg -r)
+            best=$(echo "$part1" | jq .best -r)
+            worst=$(echo "$part1" | jq .worst -r)
+            if [ $? -eq 0 ]; then
+                output="$output<text x=\"105\" y=\"$y\" $textSettings>$(emoji $avg) $avg</text>"
+                output="$output<text x=\"305\" y=\"$y\" $textSettings>$(emoji $best) $best</text>"
+                output="$output<text x=\"505\" y=\"$y\" $textSettings>$(emoji $worst) $worst</text>"
+            else
+                output="$output<text x=\"305\" y=\"$y\" fill=\"red\">🖨️❗</text>"
+            fi
         else
-            output="$output<text x=\"105\" y=\"$y\" fill=\"red\">Missing, timeout or OOM!</text>"
+            output="$output<text x=\"305\" y=\"$y\" fill=\"red\">👻</text>"
         fi
         if [ -n "$part2" ]; then
-            output="$output<text x=\"705\" y=\"$y\" fill=\"white\">$(echo "$part2" | jq .avg -r)</text>"
-            output="$output<text x=\"905\" y=\"$y\" fill=\"white\">$(echo "$part2" | jq .best -r)</text>"
-            output="$output<text x=\"1105\" y=\"$y\" fill=\"white\">$(echo "$part2" | jq .worst -r)</text>"
+            avg=$(echo "$part2" | jq .avg -r)
+            best=$(echo "$part2" | jq .best -r)
+            worst=$(echo "$part2" | jq .worst -r)
+            if [ $? -eq 0 ]; then
+                output="$output<text x=\"705\" y=\"$y\" $textSettings>$(emoji $avg) $avg</text>"
+                output="$output<text x=\"905\" y=\"$y\" $textSettings>$(emoji $best) $best</text>"
+                output="$output<text x=\"1105\" y=\"$y\" $textSettings>$(emoji $worst) $worst</text>"
+            else
+                output="$output<text x=\"905\" y=\"$y\" fill=\"red\">🖨️❗</text>"
+            fi
         else 
-            output="$output<text x=\"705\" y=\"$y\" fill=\"red\">Missing, timeout or OOM!</text>"
+            output="$output<text x=\"905\" y=\"$y\" fill=\"red\">👻</text>"
         fi
     done
     output="$output</svg>"
     echo "$output" > benchmarks-data/$year.svg
     site="$site<img src=\"https://loafey.se/advent_of_code/benchmarks/$year.svg\">"
 done
-site="$site</body></html>"
+site="$site </body></html>"
 echo "$site" > index-temp.html
