3941c7fe675353bb861ea6f8febc3eaffedf8ddc
Samuel Hammersberg <samuel@loafey.se>
2025-12-03 08:02:24 +0100

    My own caching.

--- 2025/day3.rs
+++ 2025/day3.rs
@@ -1,5 +1,9 @@
-use memoize::memoize;
 use rayon::prelude::*;
+use std::{
+    collections::HashMap,
+    hash::{DefaultHasher, Hash, Hasher},
+    sync::{LazyLock, RwLock},
+};
 
 fn input() -> impl Iterator<Item = Vec<u64>> {
     include_str!("../inputs/2025/day3.input")
@@ -7,30 +11,43 @@ fn input() -> impl Iterator<Item = Vec<u64>> {
         .map(|c| c.chars().map(|c| c as u64 - 0x30).collect::<Vec<_>>())
 }
 
-#[memoize]
-fn solve(count: u8, data: Vec<u64>) -> u64 {
+static CACHE: LazyLock<RwLock<HashMap<u64, u64>>> = LazyLock::new(Default::default);
+
+fn solve(count: u8, data: &[u64]) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    count.hash(&mut hasher);
+    data.hash(&mut hasher);
+    if let Some(cache) = CACHE.read().unwrap().get(&hasher.finish()) {
+        return *cache;
+    }
+
     if count == 1 {
-        return data.into_iter().max().unwrap();
+        return *data.iter().max().unwrap();
     }
     let mut max = 0;
     for i in 0..data.len() {
-        let mut data = data.clone();
+        let mut data = data;
 
         let v = data[i];
-        data = data[i + 1..].to_vec();
+        data = &data[i + 1..];
         if data.len() + 1 < count as usize {
             continue;
         }
         let ans = v * 10u64.pow(count as u32 - 1) + solve(count - 1, data);
         max = max.max(ans)
     }
+
+    let mut hasher = DefaultHasher::new();
+    count.hash(&mut hasher);
+    data.hash(&mut hasher);
+    CACHE.write().unwrap().insert(hasher.finish(), max);
     max
 }
 
 pub fn part1() -> u64 {
-    input().par_bridge().map(|bank| solve(2, bank)).sum()
+    input().par_bridge().map(|bank| solve(2, &bank[..])).sum()
 }
 
 pub fn part2() -> u64 {
-    input().par_bridge().map(|bank| solve(12, bank)).sum()
+    input().par_bridge().map(|bank| solve(12, &bank[..])).sum()
 }
