a3ed462aca190f4d82c196d38d3134318068bbc8
Samuel Hammersberg <samuel@loafey.se>
2025-12-03 07:47:03 +0100

    Clean up

--- 2025/day3.rs
+++ 2025/day3.rs
@@ -1,10 +1,5 @@
-use std::{
-    cell::{OnceCell, RefCell},
-    collections::HashMap,
-    hash::{DefaultHasher, Hash, Hasher},
-};
-
 use memoize::memoize;
+use rayon::prelude::*;
 
 fn input() -> impl Iterator<Item = Vec<u64>> {
     include_str!("../inputs/2025/day3.input")
@@ -12,34 +7,8 @@ fn input() -> impl Iterator<Item = Vec<u64>> {
         .map(|c| c.chars().map(|c| c as u64 - 0x30).collect::<Vec<_>>())
 }
 
-pub fn part1() -> u64 {
-    let mut sum = 0;
-    for bank in input() {
-        let mut max = 0;
-        for i in 0..bank.len() {
-            for j in i + 1..bank.len() {
-                max = max.max((bank[i] * 10) + bank[j]);
-            }
-        }
-        sum += max;
-    }
-    sum
-}
-
-#[thread_local]
-static mut CACHE: OnceCell<RefCell<HashMap<u64, u64>>> = OnceCell::new();
-
+#[memoize]
 fn solve(count: u8, data: Vec<u64>) -> u64 {
-    unsafe {
-        let mut hasher = DefaultHasher::new();
-        count.hash(&mut hasher);
-        data.hash(&mut hasher);
-        let hash = hasher.finish();
-        if let Some(res) = CACHE.get_or_init(Default::default).borrow().get(&hash) {
-            // println!("cache");
-            return *res;
-        }
-    }
     if count == 1 {
         return data.into_iter().max().unwrap();
     }
@@ -55,28 +24,13 @@ fn solve(count: u8, data: Vec<u64>) -> u64 {
         let ans = v * 10u64.pow(count as u32 - 1) + solve(count - 1, data);
         max = max.max(ans)
     }
-    let mut hasher = DefaultHasher::new();
-    count.hash(&mut hasher);
-    data.hash(&mut hasher);
-    let hash = hasher.finish();
-    unsafe {
-        CACHE.get_mut().unwrap().borrow_mut().insert(hash, max);
-    }
     max
 }
 
+pub fn part1() -> u64 {
+    input().par_bridge().map(|bank| solve(2, bank)).sum()
+}
+
 pub fn part2() -> u64 {
-    let mut sum = 0;
-    for (i, bank) in input().enumerate() {
-        unsafe {
-            if let Some(c) = CACHE.get_mut() {
-                // c.borrow_mut().clear();
-            }
-        };
-        let ans = solve(12, bank);
-        println!("bank: {i}: {}", ans);
-        sum += ans;
-    }
-    println!("3121910778619");
-    sum
+    input().par_bridge().map(|bank| solve(12, bank)).sum()
 }
