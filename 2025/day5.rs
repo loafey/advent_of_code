754846eae5ca0bd8a8f36df6606742d202034528
Samuel Hammersberg <samuel@loafey.se>
2025-12-05 06:48:28 +0100

    Day 5

--- 2025/day5.rs
+++ 2025/day5.rs
@@ -1,7 +1,50 @@
+use std::{ops::RangeInclusive, u64};
+
+fn input() -> (Vec<RangeInclusive<u64>>, Vec<u64>) {
+    let (top, bot) = include_str!("../inputs/2025/day5.input")
+        .split_once("\n\n")
+        .unwrap();
+    let top = top
+        .lines()
+        .map(|s| {
+            let (a, b) = s.split_once("-").unwrap();
+            a.parse().unwrap()..=b.parse().unwrap()
+        })
+        .collect();
+    let bot = bot.lines().map(|s| s.parse().unwrap()).collect();
+    (top, bot)
+}
+
 pub fn part1() -> u64 {
-    unimplemented!()
+    let (ranges, ingd) = input();
+    let mut ok = 0;
+    for i in ingd {
+        for r in &ranges {
+            if r.contains(&i) {
+                ok += 1;
+                break;
+            }
+        }
+    }
+    ok
 }
 
 pub fn part2() -> u64 {
-    unimplemented!()
+    let (mut ranges, _) = input();
+    let mut diff = 0;
+    ranges.sort_by_key(|r| *r.start());
+    let mut last_end = u64::MAX;
+    for r in ranges {
+        let start = if *r.start() <= last_end && last_end != u64::MAX {
+            last_end + 1
+        } else {
+            *r.start()
+        };
+        if *r.end() <= last_end && last_end != u64::MAX {
+            continue;
+        }
+        diff += (start..=*r.end()).count() as u64;
+        last_end = *r.end();
+    }
+    diff
 }
