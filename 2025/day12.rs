61ec77b04bd1c5303811f2f38f4a38e9dd86d1b8
Samuel Hammersberg <shammersberg@opera.com>
2025-12-12 10:52:04 +0100

    speedier

--- 2025/day12.rs
+++ 2025/day12.rs
@@ -1,51 +1,23 @@
-#[allow(clippy::type_complexity)]
-fn input() -> (Vec<usize>, Vec<((usize, usize), Vec<usize>)>) {
-    let mut shapes = Vec::new();
-    let mut goals = Vec::new();
-    for block in include_str!("../inputs/2025/day12.input").split("\n\n") {
-        if block.contains('#') {
-            shapes.push(
-                block
-                    .lines()
-                    .skip(1)
-                    .map(|l| l.chars().filter(|c| *c == '#').count())
-                    .sum(),
-            )
-        } else {
-            for l in block.lines() {
-                let (size, indexes) = l.split_once(':').unwrap();
-                let indexes = indexes
-                    .split_whitespace()
-                    .map(|s| s.parse().unwrap())
-                    .collect();
-                let (x, y) = size.split_once('x').unwrap();
-                let (x, y) = (x.parse().unwrap(), y.parse().unwrap());
-                goals.push(((x, y), indexes));
-            }
-        }
-    }
-    (shapes, goals)
+#[inline(always)]
+fn parse(str: &'static str) -> usize {
+    unsafe { str.parse().unwrap_unchecked() }
 }
 
-pub fn part1() -> u64 {
-    let (shapes, goals) = input();
-    let goals: Vec<(_, usize)> = goals
-        .into_iter()
-        .map(|(matrix, goals)| {
-            (
-                matrix,
-                goals
-                    .into_iter()
-                    .enumerate()
-                    .map(|(i, a)| shapes[i] * a)
-                    .sum::<usize>(),
-            )
+pub fn part1() -> usize {
+    include_str!("../inputs/2025/day12.input")
+        .split("\n\n")
+        .filter(|b| !b.contains('#'))
+        .flat_map(|block| {
+            block.lines().map(|l| {
+                let (size, indexes) = l.split_once(':').unwrap();
+                let indexes: Vec<_> = indexes.split_whitespace().map(|s| 7 * parse(s)).collect();
+                let (x, y) = size.split_once('x').unwrap();
+                let (x, y) = (parse(x), parse(y));
+                ((x, y), indexes)
+            })
         })
-        .collect::<Vec<_>>();
-    goals
-        .into_iter()
-        .map(|((x, y), indexes)| (indexes <= x * y) as u64)
-        .sum()
+        .filter(|((x, y), goals)| goals.iter().sum::<usize>() <= x * y)
+        .count()
 }
 
 pub fn part2() -> u64 {
